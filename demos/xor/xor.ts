0c55329d3e68a7bbd2a3f90f9dcf90ee6ea5f27c
Arthur Dam <arthur@mediamonks.com>
2017-11-01 20:31:15 -0700

    fixed lint errors

--- demos/xor/xor.ts
+++ demos/xor/xor.ts
@@ -1,4 +1,12 @@
-import {Graph, Array1D, NDArrayMathGPU, Session, InCPUMemoryShuffledInputProviderBuilder, SGDOptimizer, CostReduction} from '../deeplearn';
+import {
+    Array1D,
+    CostReduction,
+    Graph,
+    InCPUMemoryShuffledInputProviderBuilder,
+    NDArrayMathGPU,
+    Session,
+    SGDOptimizer
+} from '../deeplearn';
 
 const graph = new Graph();
 const math = new NDArrayMathGPU();
@@ -6,7 +14,11 @@ const math = new NDArrayMathGPU();
 const input = graph.placeholder('input', [2]);
 const y = graph.placeholder('y', [1]);
 
-const hiddenLayer = graph.layers.dense('hiddenLayer', input, 2, (x) => graph.relu(x), true);
+const hiddenLayer = graph.layers.dense(
+    'hiddenLayer',
+    input,
+    2,
+    (x) => graph.relu(x), true);
 const output = graph.layers.dense('outputLayer', hiddenLayer, 1);
 
 const costTensor = graph.meanSquaredCost(output, y);
@@ -15,25 +27,33 @@ const session = new Session(graph, math);
 const optimizer = new SGDOptimizer(0.01);
 
 const inputArray = [
-  Array1D.new([0, 0]),
-  Array1D.new([0, 1]),
-  Array1D.new([1, 0]),
-  Array1D.new([1, 1])
+    Array1D.new([0, 0]),
+    Array1D.new([0, 1]),
+    Array1D.new([1, 0]),
+    Array1D.new([1, 1])
 ];
 
 const targetArray = [
-  Array1D.new([0]),
-  Array1D.new([1]),
-  Array1D.new([1]),
-  Array1D.new([0])
+    Array1D.new([0]),
+    Array1D.new([1]),
+    Array1D.new([1]),
+    Array1D.new([0])
 ];
 
-const shuffledInputProviderBuilder = new InCPUMemoryShuffledInputProviderBuilder([inputArray, targetArray]);
-const [inputProvider, targetProvider] = shuffledInputProviderBuilder.getInputProviders();
+const shuffledInputProviderBuilder =
+    new InCPUMemoryShuffledInputProviderBuilder([
+        inputArray,
+        targetArray
+    ]);
+
+const [
+    inputProvider,
+    targetProvider
+] = shuffledInputProviderBuilder.getInputProviders();
 
 const feedEntries = [
-  { tensor: input, data: inputProvider},
-  { tensor: y, data: targetProvider}
+    {tensor: input, data: inputProvider},
+    {tensor: y, data: targetProvider}
 ];
 
 /**
@@ -41,22 +61,27 @@ const feedEntries = [
  */
 for (let i = 0; i < 1000; i += 1) {
 
-  math.scope(() => {
+    math.scope(() => {
 
-    const cost = session.train(costTensor, feedEntries, 5, optimizer, CostReduction.MEAN);
+        const cost = session.train(
+            costTensor,
+            feedEntries,
+            5,
+            optimizer,
+            CostReduction.MEAN);
 
-    if (i % 10 === 0) {
-      let costValue = cost.get();
-      console.log(costValue);
-    }
-  });
+        if (i % 10 === 0) {
+            const costValue = cost.get();
+            console.log(costValue);
+        }
+    });
 }
 
 /**
  * Test the model
  */
 for (let i = 0; i < 4; i += 1) {
-  const data = inputArray[i];
-  const val = session.eval(output, [{ tensor: input, data: data }]);
-  console.log(data.dataSync(), val.getValues())
+    const data = inputArray[i];
+    const val = session.eval(output, [{tensor: input, data}]);
+    console.log(data.dataSync(), val.getValues());
 }
