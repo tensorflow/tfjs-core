ca0cb8f29493a58d1cc7bfa4233c11548e1a7194
reiinakano <reiichiro.s.nakano@gmail.com>
2017-11-06 12:20:59 +0800

    initial commit fast-style-transfer demo

---
+++ demos/fast-style-transfer/fast-style-transfer.ts
@@ -0,0 +1,280 @@
+// tslint:disable-next-line:max-line-length
+import {Array3D, GPGPUContext, NDArrayMathCPU, NDArrayMathGPU} from '../deeplearn';
+// import * as imagenet_util from '../models/imagenet_util';
+import {TransformNet} from './net';
+import {PolymerElement, PolymerHTMLElement} from '../polymer-spec';
+
+function clamp(num: number): number {
+  return Math.min(Math.max(num, 0), 255);
+}
+
+// tslint:disable-next-line:variable-name
+export const StyleTransferDemoPolymer: new () => PolymerHTMLElement =
+    PolymerElement({
+      is: 'styletransfer-demo',
+      properties: {
+        contentNames: Array,
+        selectedContentName: String,
+        styleNames: Array,
+        selectedStyleName: String
+      }
+    });
+
+export enum ApplicationState {
+  IDLE = 1,
+  TRAINING = 2
+}
+
+const CONTENT_NAMES = ['stata', 'face', 'diana', 'Upload from file'];
+const STYLE_MAPPINGS: {[varName: string]: string} = {
+  'Udnie, Francis Picabia': 'udnie',
+  'The Scream, Edvard Munch': 'scream',
+  'La Muse, Pablo Picasso': 'la_muse',
+  'Rain Princess, Leonid Afremov': 'rain_princess',
+  'The Wave, Katsushika Hokusai': 'wave',
+  'The Wreck of the Minotaur, J.M.W. Turner': 'wreck'
+}
+const STYLE_NAMES = Object.keys(STYLE_MAPPINGS);
+
+export class StyleTransferDemo extends StyleTransferDemoPolymer {
+  // DeeplearnJS stuff
+  private math: NDArrayMathGPU;
+  private mathCPU: NDArrayMathCPU;
+  private gpgpu: GPGPUContext;
+
+  private transformNet: TransformNet;
+
+  // DOM Elements
+  private contentImgElement: HTMLImageElement;
+  private styleImgElement: HTMLImageElement;
+  private sizeSlider: any;
+
+  private canvas: HTMLCanvasElement;
+  private canvasContext: CanvasRenderingContext2D;
+  private imageData: ImageData;
+
+  private startButton: HTMLButtonElement;
+
+  private camDialog: any;
+  private stream: MediaStream;
+  private webcamVideoElement: HTMLVideoElement;
+  private takePicButton: HTMLButtonElement;
+  private closeModal: HTMLButtonElement;
+
+  private fileSelect: HTMLButtonElement;
+
+  // Polymer properties
+  private contentNames: string[];
+  private selectedContentName: string;
+  private styleNames: string[];
+  private selectedStyleName: string;
+
+  private status: string;
+
+  private applicationState: ApplicationState;
+
+  ready() {
+    // Initialize DeeplearnJS stuff
+    this.gpgpu = new GPGPUContext;
+    this.math = new NDArrayMathGPU(this.gpgpu);
+    this.mathCPU = new NDArrayMathCPU;
+
+    // Initialize polymer properties
+    this.applicationState = ApplicationState.IDLE;
+    this.status = '';
+
+    // Retrieve DOM for images
+    this.contentImgElement =
+        this.querySelector('#contentImg') as HTMLImageElement;
+    this.styleImgElement = 
+        this.querySelector('#styleImg') as HTMLImageElement;
+
+    // Render DOM for images
+    this.contentNames = CONTENT_NAMES;
+    this.selectedContentName = 'stata';
+    this.contentImgElement.src = 'images/stata.jpg';
+    this.contentImgElement.height = 250;
+
+    this.styleNames = STYLE_NAMES;
+    this.selectedStyleName = 'Udnie, Francis Picabia';
+    this.styleImgElement.src = 'images/udnie.jpg';
+    this.styleImgElement.height = 250;
+
+    this.canvas = this.querySelector('#imageCanvas') as HTMLCanvasElement;
+    this.canvas.width = 0;
+    this.canvas.height = 0;
+    this.canvasContext = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+    this.canvas.style.display = 'none';
+
+    this.initWebcamVariables();
+
+    this.sizeSlider = this.querySelector('#sizeSlider') as any;
+    this.sizeSlider.addEventListener('immediate-value-change', (event: any) => {
+      this.styleImgElement.height = this.sizeSlider.immediateValue;
+      this.contentImgElement.height = this.sizeSlider.immediateValue;
+    })
+    this.sizeSlider.addEventListener('change', (event: any) => {
+      this.styleImgElement.height = this.sizeSlider.immediateValue;
+      this.contentImgElement.height = this.sizeSlider.immediateValue;
+    })
+
+    this.fileSelect = this.querySelector('#fileSelect') as HTMLButtonElement;
+    this.fileSelect.addEventListener('change', (event: any) => {
+      const f: File = event.target.files[0];
+      const fileReader: FileReader = new FileReader;
+      fileReader.onload = ((e) => {
+        const target: FileReader = e.target as FileReader;
+        this.contentImgElement.src = target.result;
+      });
+      fileReader.readAsDataURL(f);
+      this.fileSelect.value = '';
+    });
+
+    // Add listener to drop downs
+    const contentDropdown = this.querySelector('#content-dropdown');
+    // tslint:disable-next-line:no-any
+    contentDropdown.addEventListener('iron-activate', (event: any) => {
+      if (event.detail.selected === 'Use webcam') {
+        this.openWebcamModal();
+      }
+      else if (event.detail.selected === 'Upload from file') {
+        this.fileSelect.click();
+      }
+      else {
+        this.contentImgElement.src = 'images/' + event.detail.selected + '.jpg';
+      }
+    });
+
+    const styleDropdown = this.querySelector('#style-dropdown');
+    // tslint:disable-next-line:no-any
+    styleDropdown.addEventListener('iron-activate', (event: any) => {
+
+      this.styleImgElement.src = 'images/' + STYLE_MAPPINGS[event.detail.selected] + '.jpg';
+    });
+
+    // Add listener to start
+    this.startButton = this.querySelector('#start') as HTMLButtonElement;
+    this.startButton.addEventListener('click', () => {
+      (this.querySelector('#load-error-message') as HTMLElement).style.display =
+        'none';
+      this.startButton.textContent = 'Starting style transfer.. Downloading + running model';
+      this.startButton.disabled = true;
+      this.transformNet = new TransformNet(this.math,
+        STYLE_MAPPINGS[this.selectedStyleName]);
+
+      this.transformNet.loadVariables()
+      .then(() => {
+        this.startButton.textContent = 'Processing image';
+        this.runInference();
+        this.startButton.textContent = 'Start Style Transfer';
+        this.startButton.disabled = false;
+      })
+      .catch((error) => {
+        console.log(error);
+        this.startButton.textContent = 'Start Style Transfer';
+        this.startButton.disabled = false;
+        (this.querySelector('#load-error-message') as HTMLElement).textContent = error;
+        (this.querySelector('#load-error-message') as HTMLElement).style.display =
+          'block';
+      });
+    });
+  }
+
+  private initWebcamVariables() {
+    this.camDialog = this.querySelector('#webcam-dialog');
+    this.webcamVideoElement = this.querySelector('#webcamVideo') as HTMLVideoElement;
+    this.takePicButton = this.querySelector('#takePicButton') as HTMLButtonElement;
+    this.closeModal = this.querySelector('#closeModal') as HTMLButtonElement;
+
+    // Check if webcam is even available
+    // tslint:disable-next-line:no-any
+    const navigatorAny = navigator as any;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      const contentNames = CONTENT_NAMES.slice();
+      contentNames.unshift('Use webcam');
+      this.contentNames = contentNames;
+    }
+
+    this.closeModal.addEventListener('click', () => {
+      this.stream.getTracks()[0].stop();
+    });
+
+    this.takePicButton.addEventListener('click', () => {
+      var hiddenCanvas: HTMLCanvasElement = 
+        this.querySelector('#hiddenCanvas') as HTMLCanvasElement;
+      var hiddenContext: CanvasRenderingContext2D = hiddenCanvas.getContext('2d');
+      hiddenCanvas.width = this.webcamVideoElement.width;
+      hiddenCanvas.height = this.webcamVideoElement.height;
+      hiddenContext.drawImage(this.webcamVideoElement, 0, 0, 
+        hiddenCanvas.width, hiddenCanvas.height);
+      var imageDataURL = hiddenCanvas.toDataURL('image/jpg');
+      this.contentImgElement.src = imageDataURL;
+      this.stream.getTracks()[0].stop();
+    });
+  }
+
+  private openWebcamModal() {
+    this.camDialog.open();
+    navigator.getUserMedia(
+      {
+        video: true
+      },
+      (stream) => {
+        this.stream = stream;
+        this.webcamVideoElement.src = window.URL.createObjectURL(stream);
+        this.webcamVideoElement.play();
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
+
+  runInference() {
+    
+    this.math.scope((keep, track) => {
+
+      const preprocessed = track(Array3D.fromPixels(this.contentImgElement));
+
+      const inferenceResult = this.transformNet.infer(preprocessed);
+      this.drawOnCanvas(inferenceResult);
+    });
+  }
+
+  private setCanvasShape(shape: number[]) {
+    this.canvas.width = shape[1];
+    this.canvas.height = shape[0];
+    if (shape[1] > shape[0]) {
+      this.canvas.style.width = '500px';
+      this.canvas.style.height = shape[0]/shape[1]*500 + 'px';
+    }
+    else {
+      this.canvas.style.height = '500px';
+      this.canvas.style.width = shape[1]/shape[0]*500 + 'px';
+    }
+  }
+
+  private drawOnCanvas(ndarray: Array3D) {
+    this.setCanvasShape(ndarray.shape);
+    this.imageData = this.canvasContext.createImageData(
+        this.canvas.width, this.canvas.height);
+
+    let pixelOffset = 0;
+    for (let i = 0; i < ndarray.shape[0]; i++) {
+      for (let j = 0; j < ndarray.shape[1]; j++) {
+        this.imageData.data[pixelOffset++] = clamp(ndarray.get(i, j, 0));
+        this.imageData.data[pixelOffset++] = clamp(ndarray.get(i, j, 1));
+        this.imageData.data[pixelOffset++] = clamp(ndarray.get(i, j, 2));
+        this.imageData.data[pixelOffset++] = 255;
+      }
+    }
+
+    this.canvas.style.display = '';
+    this.canvasContext.putImageData(this.imageData, 0, 0);
+  }
+}
+
+document.registerElement(StyleTransferDemo.prototype.is, StyleTransferDemo);
---
+++ demos/fast-style-transfer/net.ts
@@ -0,0 +1,121 @@
+// tslint:disable-next-line:max-line-length
+import {Scalar, Array1D, Array3D, Array4D, CheckpointLoader, NDArray, NDArrayMathGPU} from '../deeplearn';
+
+const GOOGLE_CLOUD_STORAGE_DIR =
+//    'https://storage.googleapis.com/learnjs-data/checkpoint_zoo/';
+    document.URL.substr(0,document.URL.lastIndexOf('/')) + '/ckpts/';
+
+export class TransformNet {
+  private variables: {[varName: string]: NDArray};
+
+  constructor(private math: NDArrayMathGPU, private style: string) {}
+
+  /**
+   * Loads necessary variables for SqueezeNet. Resolves the promise when the
+   * variables have all been loaded.
+   */
+  loadVariables(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const checkpointLoader =
+          new CheckpointLoader(GOOGLE_CLOUD_STORAGE_DIR + this.style + '/');
+      checkpointLoader.getAllVariables()
+      .then(variables => {
+        this.variables = variables;
+        resolve();
+      })
+      .catch((error) => reject(error));
+    });
+  }
+
+  /**
+   * Infer through TransformNet, assumes variables have been loaded.
+   *
+   * @param preprocessedInput preprocessed input Array.
+   * @return Array3D containing pixels of output img
+   */
+  infer(preprocessedInput: Array3D): Array3D {
+
+    const img = this.math.scope((keep, track) => {
+      const conv1 = this.convLayer(preprocessedInput, 1, true, 0);
+      const conv2 = this.convLayer(conv1, 2, true, 3);
+      const conv3 = this.convLayer(conv2, 2, true, 6);
+      const resid1 = this.residualBlock(conv3, 9);
+      const resid2 = this.residualBlock(resid1, 15);
+      const resid3 = this.residualBlock(resid2, 21);
+      const resid4 = this.residualBlock(resid3, 27);
+      const resid5 = this.residualBlock(resid4, 33);
+      const conv_t1 = this.convTransposeLayer(resid5, 64, 2, 39);
+      const conv_t2 = this.convTransposeLayer(conv_t1, 32, 2, 42);
+      const conv_t3 = this.convLayer(conv_t2, 1, false, 45);
+      const out_tanh = this.math.tanh(conv_t3);
+      const scaled = this.math.scalarTimesArray(Scalar.new(150), out_tanh);
+      const shifted = this.math.scalarPlusArray(Scalar.new(255./2), scaled);
+
+      return shifted;
+    });
+
+    return img;
+  }
+
+  private convLayer(input: Array3D, strides: number, 
+    relu: boolean, varId: number): Array3D {
+    const y = this.math.conv2d(input, 
+      this.variables[this.varName(varId)] as Array4D, 
+      null, [strides, strides], 'same');
+
+    const y2 = this.instanceNorm(y, varId + 1);
+
+    if (relu) {
+      return this.math.relu(y2);
+    }
+
+    return y2;
+  }
+
+  private convTransposeLayer(input: Array3D, numFilters: number,
+    strides: number, varId: number): Array3D {
+    const [height, width, ]: [number, number, number] = input.shape;
+    const newRows = height * strides;
+    const newCols = width * strides;
+    const newShape: [number, number, number] = [newRows, newCols, numFilters];
+
+    const y = this.math.conv2dTranspose(input,
+      this.variables[this.varName(varId)] as Array4D,
+      newShape, [strides, strides], 'same');
+
+    const y2 = this.instanceNorm(y, varId + 1);
+
+    const y3 = this.math.relu(y2);
+
+    return y3;
+  }
+
+  private residualBlock(input: Array3D, varId: number): Array3D {
+    const conv1 = this.convLayer(input, 1, true, varId);
+    const conv2 = this.convLayer(conv1, 1, false, varId + 3);
+    return this.math.addStrict(conv2, input); 
+  }
+
+  private instanceNorm(input: Array3D, varId: number): Array3D {
+    const [height, width, inDepth]: [number, number, number] = input.shape;
+    const moments = this.math.moments(input, [0, 1]);
+    const mu = moments.mean as Array3D;
+    const sigma_sq = moments.variance as Array3D;
+    const shift = this.variables[this.varName(varId)] as Array1D;
+    const scale = this.variables[this.varName(varId + 1)] as Array1D;
+    const epsilon = Scalar.new(1e-3);
+    const normalized = this.math.divide(this.math.sub(input, mu), 
+      this.math.sqrt(this.math.add(sigma_sq, epsilon)));
+    const shifted = this.math.add(this.math.multiply(scale, normalized), shift);
+    return shifted.as3D(height, width, inDepth);
+  }
+
+  private varName(varId: number): string {
+    if (varId === 0) {
+      return 'Variable';
+    }
+    else {
+      return 'Variable_' + varId;
+    }
+  }
+}
