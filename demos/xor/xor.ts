f93c0705bd956f2b5f56cd2208289ffaf3dc5327
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-11-07 16:06:23 +0530

    Make use of log loss

--- demos/xor/xor.ts
+++ demos/xor/xor.ts
@@ -8,23 +8,27 @@ import {
     SGDOptimizer
 } from '../deeplearn';
 
+const EPSILON = 1e-7;
+
 const graph = new Graph();
 const math = new NDArrayMathGPU();
 
 const input = graph.placeholder('input', [2]);
 const y = graph.placeholder('y', [1]);
 
-const hiddenLayer = graph.layers.dense(
-    'hiddenLayer',
-    input,
-    2,
-    (x) => graph.relu(x), true);
-const output = graph.layers.dense('outputLayer', hiddenLayer, 1);
 
-const costTensor = graph.meanSquaredCost(output, y);
+const hiddenLayer = graph.layers.dense('hiddenLayer', input, 10, (x) => graph.relu(x), true);
+const output = graph.layers.dense('outputLayer', hiddenLayer, 1, (x) => graph.sigmoid(x), true);
+
+const costTensor = graph.reduceSum(
+  graph.add(
+    graph.multiply(graph.constant([-1]), graph.multiply(y, graph.log(graph.add(output, graph.constant([EPSILON]))))),
+    graph.multiply(graph.constant([-1]), graph.multiply(graph.subtract(graph.constant([1]), y), graph.log(graph.add(graph.subtract(graph.constant([1]), output), graph.constant([EPSILON])))))
+  )
+);
 
 const session = new Session(graph, math);
-const optimizer = new SGDOptimizer(0.01);
+const optimizer = new SGDOptimizer(0.2);
 
 const inputArray = [
     Array1D.new([0, 0]),
@@ -59,29 +63,21 @@ const feedEntries = [
 /**
  * Train the model
  */
-for (let i = 0; i < 1000; i += 1) {
-
+for (let i = 0; i < 3000; i += 1) {
     math.scope(() => {
-
-        const cost = session.train(
-            costTensor,
-            feedEntries,
-            5,
-            optimizer,
-            CostReduction.MEAN);
+        const cost = session.train(costTensor, feedEntries, 4, optimizer, CostReduction.MEAN);
 
         if (i % 10 === 0) {
-            const costValue = cost.get();
-            console.log(costValue);
+          let costValue = cost.get();
+          console.log(costValue);
+          /**
+           * Test the model
+           */
+          for (let i = 0; i < 4; i += 1) {
+            const data = inputArray[i];
+            const val = session.eval(output, [{ tensor: input, data: data }]);
+            console.log(data.dataSync(), val.getValues())
+          }
         }
-    });
-}
-
-/**
- * Test the model
- */
-for (let i = 0; i < 4; i += 1) {
-    const data = inputArray[i];
-    const val = session.eval(output, [{tensor: input, data}]);
-    console.log(data.dataSync(), val.getValues());
+  });
 }
