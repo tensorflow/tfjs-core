871d04d5bf8577f269ed62b5eb6c7dda885437c0
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-11-07 22:13:56 +0530

    Fix travis linting issues

--- demos/xor/xor.ts
+++ demos/xor/xor.ts
@@ -1,12 +1,12 @@
 import {
-    Array1D,
-    CostReduction,
-    Graph,
-    InCPUMemoryShuffledInputProviderBuilder,
-    NDArrayMathGPU,
-    Session,
-    SGDOptimizer
-} from '../deeplearn';
+  Array1D,
+  Graph,
+  InCPUMemoryShuffledInputProviderBuilder,
+  NDArrayMathGPU,
+  Session,
+  SGDOptimizer,
+  Tensor
+} from 'deeplearn';
 
 const EPSILON = 1e-7;
 
@@ -18,9 +18,9 @@ const y = graph.placeholder('y', [1]);
 
 
 const hiddenLayer = graph.layers.dense(
-  'hiddenLayer', input, 10, (x) => graph.relu(x), true);
+  'hiddenLayer', input, 10, (x: Tensor) => graph.relu(x), true);
 const output = graph.layers.dense(
-  'outputLayer', hiddenLayer, 1, (x) => graph.sigmoid(x), true);
+  'outputLayer', hiddenLayer, 1, (x: Tensor) => graph.sigmoid(x), true);
 
 const costTensor = graph.reduceSum(
   graph.add(
@@ -50,53 +50,49 @@ const session = new Session(graph, math);
 const optimizer = new SGDOptimizer(0.2);
 
 const inputArray = [
-    Array1D.new([0, 0]),
-    Array1D.new([0, 1]),
-    Array1D.new([1, 0]),
-    Array1D.new([1, 1])
+  Array1D.new([0, 0]),
+  Array1D.new([0, 1]),
+  Array1D.new([1, 0]),
+  Array1D.new([1, 1])
 ];
 
 const targetArray = [
-    Array1D.new([0]),
-    Array1D.new([1]),
-    Array1D.new([1]),
-    Array1D.new([0])
+  Array1D.new([0]),
+  Array1D.new([1]),
+  Array1D.new([1]),
+  Array1D.new([0])
 ];
 
 const shuffledInputProviderBuilder =
-    new InCPUMemoryShuffledInputProviderBuilder([
-        inputArray,
-        targetArray
-    ]);
+  new InCPUMemoryShuffledInputProviderBuilder([
+    inputArray,
+    targetArray
+  ]);
 
 const [
-    inputProvider,
-    targetProvider
+  inputProvider,
+  targetProvider
 ] = shuffledInputProviderBuilder.getInputProviders();
 
 const feedEntries = [
-    {tensor: input, data: inputProvider},
-    {tensor: y, data: targetProvider}
+  {tensor: input, data: inputProvider},
+  {tensor: y, data: targetProvider}
 ];
 
 /**
  * Train the model
  */
-for (let i = 0; i < 3000; i += 1) {
-    math.scope(() => {
-        const cost = session.train(costTensor, feedEntries, 4, optimizer, CostReduction.MEAN);
-
-        if (i % 10 === 0) {
-          let costValue = cost.get();
-          console.log(costValue);
-          /**
-           * Test the model
-           */
-          for (let i = 0; i < 4; i += 1) {
-            const data = inputArray[i];
-            const val = session.eval(output, [{ tensor: input, data: data }]);
-            console.log(data.dataSync(), val.getValues())
-          }
-        }
+for (let i = 0; i < 1100; i += 1) {
+  math.scope(() => {
+    session.train(costTensor, feedEntries, 4, optimizer);
   });
+}
+
+/**
+ * Test the model
+ */
+for (let i = 0; i < 4; i += 1) {
+  const inputData = inputArray[i];
+  const val = session.eval(output, [{ tensor: input, data: inputData }]);
+  console.log(inputData.dataSync(), val.getValues());
 }
