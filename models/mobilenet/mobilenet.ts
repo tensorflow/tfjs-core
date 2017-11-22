38808462315702e41310ab1b77ee9d279146ac37
andy <experiencor@gmail.com>
2017-11-22 16:25:11 +0800

    post-processing in GPU

--- models/mobilenet/mobilenet.ts
+++ models/mobilenet/mobilenet.ts
@@ -29,6 +29,7 @@ export class MobileNet implements Model {
   private HALF_TWO_FIVE_FIVE  = Scalar.new(255.0/2);
   private ONE = Scalar.ONE;
   private THRESHOLD = 0.3;
+  private THRESHOLD_SCALAR = Scalar.new(this.THRESHOLD);
   private ANCHORS: number[] = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828];
 
   constructor(private math: NDArrayMath) {
@@ -39,14 +40,16 @@ export class MobileNet implements Model {
     initializeGPU(
         (this.math as NDArrayMathGPU).getGPGPUContext(),  
         (this.math as NDArrayMathGPU).getTextureManager());
+
+        //this.math.enableDebugMode();
   }
 
   /**
    * Loads necessary variables for MobileNet.
    */
   async load(): Promise<void> {
-    const checkpointLoader =
-        new CheckpointLoader('weights/');
+    //const checkpointLoader = new CheckpointLoader('https://raw.githubusercontent.com/experiencor/model-zoo/master/mobilenet/');
+    const checkpointLoader = new CheckpointLoader('weights/');
     this.variables = await checkpointLoader.getAllVariables();
   }
 
@@ -58,118 +61,151 @@ export class MobileNet implements Model {
    * @param input un-preprocessed input Array.
    * @return Named activations and the pre-softmax logits.
    */
-  async predict(input: Array3D): Promise<{
-    netout: Array4D
-  }> {
+  async predict(input: Array3D): Promise<Array4D> {
     // Keep a map of named activations for rendering purposes.
     const netout = this.math.scope((keep) => {
       // Preprocess the input.
       const preprocessedInput = this.math.subtract(this.math.arrayDividedByScalar(input, this.HALF_TWO_FIVE_FIVE), this.ONE) as Array3D;
 
-      const x1  = this._conv_block(preprocessedInput, [2, 2])
-      const x2  = this._depthwise_conv_block(x1,  [1, 1], 1)
+      const x1  = this.convBlock(preprocessedInput, [2, 2])
+      const x2  = this.depthwiseConvBlock(x1,  [1, 1], 1)
 
-      const x3  = this._depthwise_conv_block(x2,  [2, 2], 2)
-      const x4  = this._depthwise_conv_block(x3,  [1, 1], 3)
+      const x3  = this.depthwiseConvBlock(x2,  [2, 2], 2)
+      const x4  = this.depthwiseConvBlock(x3,  [1, 1], 3)
 
-      const x5  = this._depthwise_conv_block(x4,  [2, 2], 4)
-      const x6  = this._depthwise_conv_block(x5,  [1, 1], 5)
+      const x5  = this.depthwiseConvBlock(x4,  [2, 2], 4)
+      const x6  = this.depthwiseConvBlock(x5,  [1, 1], 5)
 
-      const x7  = this._depthwise_conv_block(x6,  [2, 2], 6)
-      const x8  = this._depthwise_conv_block(x7,  [1, 1], 7)
-      const x9  = this._depthwise_conv_block(x8,  [1, 1], 8)
-      const x10 = this._depthwise_conv_block(x9,  [1, 1], 9)
-      const x11 = this._depthwise_conv_block(x10, [1, 1], 10)
-      const x12 = this._depthwise_conv_block(x11, [1, 1], 11)
+      const x7  = this.depthwiseConvBlock(x6,  [2, 2], 6)
+      const x8  = this.depthwiseConvBlock(x7,  [1, 1], 7)
+      const x9  = this.depthwiseConvBlock(x8,  [1, 1], 8)
+      const x10 = this.depthwiseConvBlock(x9,  [1, 1], 9)
+      const x11 = this.depthwiseConvBlock(x10, [1, 1], 10)
+      const x12 = this.depthwiseConvBlock(x11, [1, 1], 11)
 
-      const x13 = this._depthwise_conv_block(x12, [2, 2], 12)
-      const x14 = this._depthwise_conv_block(x13, [1, 1], 13) 
+      const x13 = this.depthwiseConvBlock(x12, [2, 2], 12)
+      const x14 = this.depthwiseConvBlock(x13, [1, 1], 13) 
 
-      const x15 = this.math.conv2d(x14, // input
-                                   this.variables['conv_23/kernel'] as Array4D, // filter
-                                   this.variables['conv_23/bias'] as Array1D, // bias
-                                   [1,1], // stride
-                                   'same'); // padding      
+      const x15 = this.math.conv2d(x14,
+                                   this.variables['conv_23/kernel'] as Array4D,
+                                   this.variables['conv_23/bias'] as Array1D,
+                                   [1,1],
+                                   'same');    
  
       return x15.as4D(13, 13, 5, 6);
     });
 
-    return {netout: netout};
+    return netout;
     } 
 
-  private _conv_block(inputs: Array3D, strides: [number, number]) { 
+  private convBlock(inputs: Array3D, strides: [number, number]) { 
 
-    const x1 = this.math.conv2d(inputs, // input
-                                this.variables['conv1/kernel'] as Array4D, // filter
-                                null, // bias
-                                strides, // stride
-                                'same'); // padding
+    const x1 = this.math.conv2d(inputs,
+                                this.variables['conv1/kernel'] as Array4D,
+                                null,
+                                strides,
+                                'same');
 
     const x2 = this.math.batchNormalization3D(x1,
-                                              this.variables['conv1_bn/moving_mean'] as Array1D, // mean
-                                              this.variables['conv1_bn/moving_variance'] as Array1D, // variance
+                                              this.variables['conv1_bn/moving_mean'] as Array1D,
+                                              this.variables['conv1_bn/moving_variance'] as Array1D,
                                               .001,
-                                              this.variables['conv1_bn/gamma'] as Array1D, // gamma
-                                              this.variables['conv1_bn/beta'] as Array1D); // beta
+                                              this.variables['conv1_bn/gamma'] as Array1D,
+                                              this.variables['conv1_bn/beta'] as Array1D);
 
     return this.math.clip(x2, 0, 6); // simple implementation of Relu6
   }
 
-  private _depthwise_conv_block(inputs: Array3D,
-                                strides: [number, number],
-                                blockID: number) {
+  private depthwiseConvBlock(inputs: Array3D,
+                             strides: [number, number],
+                             blockID: number) {
 
-    const x1 = this.math.depthwiseConv2D(inputs, // input
-                                         this.variables['conv_dw_' + blockID + '/depthwise_kernel'] as Array4D, // filter
-                                         strides, // strides
-                                         'same') as Array3D; // padding
+    const x1 = this.math.depthwiseConv2D(inputs,
+                                         this.variables['conv_dw_' + blockID + '/depthwise_kernel'] as Array4D,
+                                         strides,
+                                         'same') as Array3D;
 
     const x2 = this.math.batchNormalization3D(x1, 
-                                              this.variables['conv_dw_' + blockID + '_bn/moving_mean'] as Array1D, // mean
-                                              this.variables['conv_dw_' + blockID + '_bn/moving_variance'] as Array1D, // variance
+                                              this.variables['conv_dw_' + blockID + '_bn/moving_mean'] as Array1D,
+                                              this.variables['conv_dw_' + blockID + '_bn/moving_variance'] as Array1D,
                                               .001,
-                                              this.variables['conv_dw_' + blockID + '_bn/gamma'] as Array1D, // gamma
-                                              this.variables['conv_dw_' + blockID + '_bn/beta'] as Array1D); // beta
+                                              this.variables['conv_dw_' + blockID + '_bn/gamma'] as Array1D,
+                                              this.variables['conv_dw_' + blockID + '_bn/beta'] as Array1D);
 
     const x3 = this.math.clip(x2, 0, 6)
 
-    const x4 = this.math.conv2d(x3, // input
-                                this.variables['conv_pw_' + blockID + '/kernel'] as Array4D, // filter
-                                null, // bias
-                                [1, 1], // strides
-                                'same'); // padding
+    const x4 = this.math.conv2d(x3,
+                                this.variables['conv_pw_' + blockID + '/kernel'] as Array4D,
+                                null,
+                                [1, 1],
+                                'same');
 
     const x5 = this.math.batchNormalization3D(x4, 
-                                              this.variables['conv_pw_' + blockID + '_bn/moving_mean'] as Array1D, // mean
-                                              this.variables['conv_pw_' + blockID + '_bn/moving_variance'] as Array1D, // variance
+                                              this.variables['conv_pw_' + blockID + '_bn/moving_mean'] as Array1D,
+                                              this.variables['conv_pw_' + blockID + '_bn/moving_variance'] as Array1D,
                                               .001,
-                                              this.variables['conv_pw_' + blockID + '_bn/gamma'] as Array1D, // gamma
-                                              this.variables['conv_pw_' + blockID + '_bn/beta'] as Array1D); // beta
+                                              this.variables['conv_pw_' + blockID + '_bn/gamma'] as Array1D,
+                                              this.variables['conv_pw_' + blockID + '_bn/beta'] as Array1D);
 
     return this.math.clip(x5, 0, 6)
   }
 
-  interpret_netout(netout: Array4D){ 
+  async interpretNetout(netout: Array4D): Promise<BoundingBox[]> { 
     // interpret the output by the network
     var [GRID_H, GRID_W, BOX, CLASS] = netout.shape;
-    const values = netout.getValues() as Float32Array;
-    const boxes: BoundingBox[] = new Array<BoundingBox>();
+    const boxes: BoundingBox[] = [];
 
     CLASS = CLASS - 5;
 
-    for (var row = 0; row < GRID_H; row++) {
+    var confidence = this.math.sigmoid(this.math.slice4D(netout, [0, 0, 0, 4], [GRID_H, GRID_W, BOX, 1]))
+    var classes = this.math.softmax(this.math.slice4D(netout, [0, 0, 0, 5], [GRID_H, GRID_W, BOX, CLASS]))
+
+    classes = this.math.multiply(classes, confidence) as Array4D;
+    const mask = this.math.step(this.math.relu(this.math.subtract(classes, this.THRESHOLD_SCALAR)));
+    classes = this.math.multiply(classes, mask) as Array4D;
+
+    const objectLikelihood = this.math.sum(classes, 3);
+    const objectLikelihoodValues = objectLikelihood.getValues();
+
+    for (var i = 0; i < objectLikelihoodValues.length; i++) {
+      if (objectLikelihoodValues[i] > 0) {
+        var [row, col, box] = objectLikelihood.indexToLoc(i);
+
+        const conf = confidence.get(row, col, box, 0)
+        const probs = this.math.slice4D(classes, [row, col, box, 0], [1, 1, 1, CLASS]).getValues() as Float32Array;
+
+        var xywh = this.math.slice4D(netout, [row, col, box, 0], [1, 1, 1, 4]).getValues();
+
+        var x = xywh[0];
+        var y = xywh[1];
+        var w = xywh[2];
+        var h = xywh[3];
+        
+        x = (col + this.sigmoid(x)) / GRID_W;
+        y = (row + this.sigmoid(y)) / GRID_H;
+        w = this.ANCHORS[2 * box + 0] * Math.exp(w) / GRID_W;
+        h = this.ANCHORS[2 * box + 1] * Math.exp(h) / GRID_H;
+
+        boxes.push(new BoundingBox(x, y, w, h, conf, probs));        
+      }
+    }
+
+    /*for (var row = 0; row < GRID_H; row++) {
       for (var col = 0; col < GRID_W; col++) {
         for (var box = 0; box < BOX; box++) {
           const l_bound = netout.locToIndex([row, col, box, 0]);
-         
           // get and adjust the confidence of the box
-          var confidence = this.sigmoid(values[l_bound+4]);
+          var confidence = this.math.sigmoid(this.math.slice4D(netout, [row, col, box, 4], [1, 1, 1, 1])).flatten();
 
           // get and adjust class likelihoods
-          var classes = values.slice(l_bound+5, l_bound+5+CLASS);
-          classes = this.sofmax(classes).map((x) => (x * confidence)).map((x) => x > this.THRESHOLD ? x : 0);
+          var classes = this.math.slice4D(netout, [row, col, box, 5], [1, 1, 1, CLASS]).flatten();
+          classes = this.math.multiply(this.math.softmax(classes), confidence).flatten();
+          const mask = this.math.step(this.math.relu(this.math.subtract(classes, this.THRESHOLD_SCALAR)));
+          classes = this.math.multiply(classes, mask).flatten();
 
-          if (classes.reduce(( a, b ) => a + b, 0) > 0) {
+          const objectLikelihood = this.math.sum(classes).getValues()[0];
+
+          if (objectLikelihood > 0) {
             var x = values[l_bound];
             var y = values[l_bound+1];
             var w = values[l_bound+2];
@@ -180,11 +216,11 @@ export class MobileNet implements Model {
             w = this.ANCHORS[2 * box + 0] * Math.exp(w) / GRID_W;
             h = this.ANCHORS[2 * box + 1] * Math.exp(h) / GRID_H;
 
-            boxes.push(new BoundingBox(x, y, w, h, confidence, classes));
+            boxes.push(new BoundingBox(x, y, w, h, confidence.getValues()[0], classes.getValues() as Float32Array));
           }
         }
       }
-    }
+    }*/
 
     // suppress nonmaximal boxes
     for (var cls = 0; cls < CLASS; cls++) {
@@ -210,7 +246,7 @@ export class MobileNet implements Model {
         }
       }
 
-    }
+    }  
 
     // obtain the most likely boxes
     var likelyBoxes = []
@@ -226,14 +262,7 @@ export class MobileNet implements Model {
 
   private sigmoid(x: number): number {
     return 1./ (1. + Math.exp(-x));
-  }
-
-  private sofmax(x: Float32Array): Float32Array {
-    const expX = x.map(Math.exp);
-    const sumX = expX.reduce((a,b) => (a+b));
-
-    return expX.map((x) => (x/sumX));
-  }
+  }    
 
   dispose() {
     this.preprocessOffset.dispose();
