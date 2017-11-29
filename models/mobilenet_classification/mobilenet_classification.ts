390d5487ea5bd76347d4ba672634ad6ad4505ec3
andy <experiencor@gmail.com>
2017-11-30 03:44:16 +0800

    removed local classification weights

--- models/mobilenet_classification/mobilenet_classification.ts
+++ models/mobilenet_classification/mobilenet_classification.ts
@@ -18,7 +18,7 @@
 import {Array1D, Array3D, Array4D, CheckpointLoader, initializeGPU, Model, NDArray, NDArrayMathCPU, NDArrayMath, NDArrayMathGPU, Scalar} from 'deeplearn';
 
 import {IMAGENET_CLASSES} from './imagenet_classes'; 
-//const GOOGLE_CLOUD_STORAGE_DIR = 'https://storage.googleapis.com/learnjs-data/checkpoint_zoo/';
+const GOOGLE_CLOUD_STORAGE_DIR = 'https://storage.googleapis.com/learnjs-data/checkpoint_zoo/mobilenet_v1_1.0_224/';
    
 export class MobileNet implements Model {  
   private variables: {[varName: string]: NDArray};
@@ -43,8 +43,7 @@ export class MobileNet implements Model {
    * Loads necessary variables for MobileNet.
    */
   async load(): Promise<void> {
-    //const checkpointLoader = new CheckpointLoader('https://raw.githubusercontent.com/experiencor/model-zoo/master/mobilenet/');
-    const checkpointLoader = new CheckpointLoader('weights/');
+    const checkpointLoader = new CheckpointLoader(GOOGLE_CLOUD_STORAGE_DIR);
     this.variables = await checkpointLoader.getAllVariables();
   }
 
