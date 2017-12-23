c1ef39d61d035b5be889508b7a793f3134da6c7e
Nikhil Thorat <nsthorat@google.com>
2017-12-23 15:26:41 -0500

    save work

--- src/math/math_test.ts
+++ src/math/math_test.ts
@@ -447,9 +447,9 @@ import {Array1D, Array2D, Array3D, Scalar} from './ndarray';
   };
 
   test_util.describeMathCPU('valueAndGradients', [tests]);
-  // test_util.describeMathGPU('valueAndGradients', [tests], [
-  //   {'WEBGL_FLOAT_TEXTURE_ENABLED': true, 'WEBGL_VERSION': 1},
-  //   {'WEBGL_FLOAT_TEXTURE_ENABLED': true, 'WEBGL_VERSION': 2},
-  //   {'WEBGL_FLOAT_TEXTURE_ENABLED': false, 'WEBGL_VERSION': 1}
-  // ]);
+  test_util.describeMathGPU('valueAndGradients', [tests], [
+    {'WEBGL_FLOAT_TEXTURE_ENABLED': true, 'WEBGL_VERSION': 1},
+    {'WEBGL_FLOAT_TEXTURE_ENABLED': true, 'WEBGL_VERSION': 2},
+    {'WEBGL_FLOAT_TEXTURE_ENABLED': false, 'WEBGL_VERSION': 1}
+  ]);
 }
