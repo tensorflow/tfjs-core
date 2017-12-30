36aa736c643e08fd58615cd755b793d9b384cdde
Daniel Smilkov <dsmilkov@gmail.com>
2017-12-30 15:31:15 -0500

    review

--- src/math/binaryop_test.ts
+++ src/math/binaryop_test.ts
@@ -17,7 +17,6 @@
 
 import * as test_util from '../test_util';
 import {MathTests} from '../test_util';
-
 import {Array1D, Array2D, Scalar} from './ndarray';
 
 // math.prelu
@@ -83,13 +82,40 @@ import {Array1D, Array2D, Scalar} from './ndarray';
 // math.maximum
 {
   const tests: MathTests = it => {
-    it('basic', math => {
+    it('float32 and float32', math => {
       const a = Array1D.new([0.5, 3, -0.1, -4]);
       const b = Array1D.new([0.2, 0.4, 0.25, 0.15]);
       const result = math.maximum(a, b);
 
       expect(result.shape).toEqual(a.shape);
       test_util.expectArraysClose(result, [0.5, 3, 0.25, 0.15]);
+    });
+
+    it('int32 and int32', math => {
+      const a = Array1D.new([1, 5, 2, 3], 'int32');
+      const b = Array1D.new([2, 3, 1, 4], 'int32');
+      const result = math.maximum(a, b);
+
+      expect(result.shape).toEqual(a.shape);
+      expect(result.dtype).toBe('int32');
+      test_util.expectArraysEqual(result, [2, 5, 2, 4]);
+    });
+
+    it('bool and bool', math => {
+      const a = Array1D.new([true, false, false, true], 'bool');
+      const b = Array1D.new([false, false, true, true], 'bool');
+      const result = math.maximum(a, b);
+
+      expect(result.shape).toEqual(a.shape);
+      expect(result.dtype).toBe('bool');
+      test_util.expectArraysEqual(result, [true, false, true, true]);
+    });
+
+    it('different dtypes throws error', math => {
+      const a = Array1D.new([true, false, false, true], 'float32');
+      const b = Array1D.new([false, false, true, true], 'int32');
+      // tslint:disable-next-line:no-any
+      expect(() => math.maximum(a, b as any)).toThrowError();
     });
 
     it('propagates NaN', math => {
@@ -149,13 +175,40 @@ import {Array1D, Array2D, Scalar} from './ndarray';
 // math.minimum
 {
   const tests: MathTests = it => {
-    it('basic', math => {
+    it('float32 and float32', math => {
       const a = Array1D.new([0.5, 3, -0.1, -4]);
       const b = Array1D.new([0.2, 0.4, 0.25, 0.15]);
       const result = math.minimum(a, b);
 
       expect(result.shape).toEqual(a.shape);
       test_util.expectArraysClose(result, [0.2, 0.4, -0.1, -4]);
+    });
+
+    it('int32 and int32', math => {
+      const a = Array1D.new([1, 5, 2, 3], 'int32');
+      const b = Array1D.new([2, 3, 1, 4], 'int32');
+      const result = math.minimum(a, b);
+
+      expect(result.shape).toEqual(a.shape);
+      expect(result.dtype).toBe('int32');
+      test_util.expectArraysEqual(result, [1, 3, 1, 3]);
+    });
+
+    it('bool and bool', math => {
+      const a = Array1D.new([true, false, false, true], 'bool');
+      const b = Array1D.new([false, false, true, true], 'bool');
+      const result = math.minimum(a, b);
+
+      expect(result.shape).toEqual(a.shape);
+      expect(result.dtype).toBe('bool');
+      test_util.expectArraysEqual(result, [false, false, false, true]);
+    });
+
+    it('different dtypes throws error', math => {
+      const a = Array1D.new([true, false, false, true], 'float32');
+      const b = Array1D.new([false, false, true, true], 'int32');
+      // tslint:disable-next-line:no-any
+      expect(() => math.minimum(a, b as any)).toThrowError();
     });
 
     it('propagates NaN', math => {
