9151d0ee1e5539444eedeaf85897b48c634cde1d
Nick Kreeger <kreeger@google.com>
2018-01-10 22:05:11 -0800

    add test file.

---
+++ src/math/logical_ops_test.ts
@@ -0,0 +1,36 @@
+/**
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as test_util from '../test_util';
+import {MathTests} from '../test_util';
+
+import {Array1D} from './ndarray';
+
+const tests: MathTests = it => {
+  it('Should do something', math => {
+    const a = Array1D.new([1, 0, 0], 'int32');
+    const b = Array1D.new([0, 1, 0], 'int32');
+    console.log(math.logicalOr(a, b).dataSync());
+  });
+
+  test_util.describeMathCPU('pad1D', [tests]);
+  test_util.describeMathGPU('pad1D', [tests], [
+    {'WEBGL_FLOAT_TEXTURE_ENABLED': true, 'WEBGL_VERSION': 1},
+    {'WEBGL_FLOAT_TEXTURE_ENABLED': true, 'WEBGL_VERSION': 2},
+    {'WEBGL_FLOAT_TEXTURE_ENABLED': false, 'WEBGL_VERSION': 1}
+  ]);
+};
