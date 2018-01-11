9af5eaad514afe45dc7dbdfa0c5f0a84ff924622
Nick Kreeger <kreeger@google.com>
2018-01-11 10:40:39 -0800

    wip

--- src/math/logical_ops_test.ts
+++ src/math/logical_ops_test.ts
@@ -17,20 +17,59 @@
 
 import * as test_util from '../test_util';
 import {MathTests} from '../test_util';
-import {Array1D} from './ndarray';
+import {Array1D, Array2D, NDArray} from './ndarray';
+
+function printArray(array: NDArray) {
+  const output = [];
+  const values = array.dataSync();
+  for (let i = 0; i < values.length; i++) {
+    output.push(values[i]);
+  }
+  console.log(output);
+}
 
 {
   const tests: MathTests = it => {
-    it('Should do something KREEGER', math => {
+    it('Should handle Array1D.', math => {
+      const a = Array1D.new([1, 0, 0]);
+      const b = Array1D.new([0, 1, 0]);
+      test_util.expectArraysClose(math.logicalOr(a, b), [1, 1, 0]);
+    });
+
+    it('Should handle mismatched Array1D shapes', math => {
+      const a = Array1D.new([1, 0]);
+      const b = Array1D.new([0, 1, 0]);
+      const f = () => {
+        math.logicalOr(a, b);
+      };
+      expect(f).toThrowError();
+    });
+
+    it('Should handle mismatched Array1D types', math => {
       const a = Array1D.new([1, 0, 0], 'int32');
-      const b = Array1D.new([0, 1, 0], 'int32');
-      console.log('hi');
-      console.log(math.logicalOr(a, b).dataSync());
+      const b = Array1D.new([0, 1, 0], 'float32');
+      const f = () => {
+        math.logicalOr(a, b);
+      };
+      expect(f).toThrowError();
+    });
+
+    it('Should handle NaNs in Array1D', math => {
+      const a = Array1D.new([1, NaN, 0]);
+      const b = Array1D.new([0, 0, NaN]);
+      printArray(math.logicalOr(a, b));
+      // test_util.expectArraysClose(math.logicalOr(a, b), [1, 1, 0]);
+    });
+
+    it('Should handle Array2D', math => {
+      const a = Array2D.new([2, 3], [[1, 0, 1], [0, 0, 0]]);
+      const b = Array2D.new([2, 3], [[0, 0, 0], [0, 1, 0]]);
+      test_util.expectArraysClose(math.logicalOr(a, b), [1, 0, 1, 0, 1, 0]);
     });
   };
 
-  test_util.describeMathCPU('logicalOr', [tests]);
-  test_util.describeMathGPU('logicalOr', [tests], [
+  test_util.describeMathCPU('logicalOr KREEGER', [tests]);
+  test_util.describeMathGPU('logicalOr KREEGER', [tests], [
     {'WEBGL_FLOAT_TEXTURE_ENABLED': true, 'WEBGL_VERSION': 1},
     {'WEBGL_FLOAT_TEXTURE_ENABLED': true, 'WEBGL_VERSION': 2},
     {'WEBGL_FLOAT_TEXTURE_ENABLED': false, 'WEBGL_VERSION': 1}
