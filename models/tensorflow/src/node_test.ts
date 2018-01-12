88933e25093c15ceeeb0daa54554c9f182dc7f6a
Ping Yu <4018+pyu10055@users.noreply.github.com>
2018-01-12 15:00:37 -0800

    add more unit tests

---
+++ models/tensorflow/src/node_test.ts
@@ -0,0 +1,79 @@
+/**
+ * @license
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+import {getBoolParam, getNumericArrayParam, getStringParam, getTensorParam, performMathOp, tensorflow} from './index';
+
+describe('node', () => {
+  let node: tensorflow.NodeDef;
+  beforeEach(() => {
+    node = new tensorflow.NodeDef();
+    node.name = 'test';
+    node.op = 'Const';
+    node.input = ['input1'];
+  });
+
+  describe('performMathOp', () => {});
+
+  describe('getBoolParam', () => {
+    it('should load correctly', () => {
+      const boolAttr = new tensorflow.AttrValue();
+      boolAttr.b = false;
+      node.attr['bool'] = boolAttr;
+      expect(getBoolParam(node.attr, 'bool', true)).toBe(false);
+    });
+  });
+
+  describe('getStringParam', () => {
+    it('should load correctly', () => {
+      const testString = 'test';
+      const array = new Uint8Array(new ArrayBuffer(testString.length));
+
+      for (let i = 0; i < testString.length; i++) {
+        array[i] = testString.charCodeAt(i);
+      }
+      const strAttr = new tensorflow.AttrValue();
+      strAttr.s = array;
+      node.attr['string'] = strAttr;
+      expect(getStringParam(node.attr, 'string', 'default')).toBe(testString);
+    });
+  });
+
+  describe('getTensorParam', () => {
+    it('should load correctly', () => {
+      const testTensor = new tensorflow.Tensor();
+      const strAttr = new tensorflow.AttrValue();
+      strAttr.tensor = testTensor;
+      node.attr['tensor'] = strAttr;
+      expect(getTensorParam(node.attr, 'tensor', new tensorflow.Tensor()))
+          .toBe(testTensor);
+    });
+  });
+  describe('getNumericArrayParam', () => {
+    it('should load correctly', () => {
+      const strAttr = new tensorflow.AttrValue({list: {f: [1.0]}});
+      node.attr['array'] = strAttr;
+      expect(getNumericArrayParam(node.attr, 'array', [])).toEqual([1.0]);
+    });
+    it('should allow range input correctly', () => {
+      const strAttr =
+          new tensorflow.AttrValue({list: {f: [1.0, 2.0, 2.0, 1.0]}});
+      node.attr['array'] = strAttr;
+      expect(getNumericArrayParam(node.attr, 'array', [], 1, 3)).toEqual([
+        2.0, 2.0
+      ]);
+    });
+  });
+});
--- models/tensorflow/src/util_test.ts
+++ models/tensorflow/src/util_test.ts
@@ -15,19 +15,82 @@
  * =============================================================================
  */
 
-import {unescape} from './index';
+import {tensorflow, tensorToNDArray, unescape} from './index';
 
-describe('util.unescape', () => {
-  it('should unescape octal correctly', () => {
-    expect(unescape('\\001\\002\\343').length).toBe(3);
+describe('util', () => {
+  describe('unescape', () => {
+    it('should unescape octal correctly', () => {
+      expect(unescape('\\001\\002\\343').length).toBe(3);
+    });
+
+    it('should unescape special chars', () => {
+      expect(unescape('\\001\\377\\n\\"\\\\\\r\\\'').length).toBe(7);
+      expect(unescape(
+                 '\\001\\000\\000\\000\\r\\000\\000' +
+                 '\\000\\r\\000\\000\\000\\000\\002\\000\\000')
+                 .length)
+          .toBe(16);
+    });
   });
 
-  it('should unescape special chars', () => {
-    expect(unescape('\\001\\377\\n\\"\\\\\\r\\\'').length).toBe(7);
-    expect(unescape(
-               '\\001\\000\\000\\000\\r\\000\\000' +
-               '\\000\\r\\000\\000\\000\\000\\002\\000\\000')
-               .length)
-        .toBe(16);
+  describe('tensorToNDArray', () => {
+    let tensor: tensorflow.Tensor;
+    beforeEach(() => {
+      tensor = new tensorflow.Tensor();
+      tensor.dtype = tensorflow.DataType.DT_FLOAT;
+      tensor.tensorContent = new Uint8Array(new Float32Array([1.0]).buffer);
+      tensor.tensorShape = new tensorflow.TensorShape({dim: []});
+    });
+
+    it('should load scalar correctly', () => {
+      const ndArray = tensorToNDArray(tensor);
+      expect(ndArray.rank).toBe(0);
+      expect(ndArray.dtype).toBe('float32');
+      expect(ndArray.dataSync().length).toEqual(1);
+      expect(ndArray.dataSync()[0]).toEqual(1.0);
+    });
+
+    it('should load 1D array correctly', () => {
+      tensor.tensorShape = new tensorflow.TensorShape({dim: [{size: 1}]});
+
+      const ndArray = tensorToNDArray(tensor);
+      expect(ndArray.rank).toBe(1);
+      expect(ndArray.dtype).toBe('float32');
+      expect(ndArray.dataSync().length).toEqual(1);
+      expect(ndArray.dataSync()[0]).toEqual(1.0);
+    });
+
+    it('should load 2D array correctly', () => {
+      tensor.tensorShape =
+          new tensorflow.TensorShape({dim: [{size: 1}, {size: 1}]});
+
+      const ndArray = tensorToNDArray(tensor);
+      expect(ndArray.rank).toBe(2);
+      expect(ndArray.dtype).toBe('float32');
+      expect(ndArray.dataSync().length).toEqual(1);
+      expect(ndArray.dataSync()[0]).toEqual(1.0);
+    });
+
+    it('should load 3D array correctly', () => {
+      tensor.tensorShape =
+          new tensorflow.TensorShape({dim: [{size: 1}, {size: 1}, {size: 1}]});
+
+      const ndArray = tensorToNDArray(tensor);
+      expect(ndArray.rank).toBe(3);
+      expect(ndArray.dtype).toBe('float32');
+      expect(ndArray.dataSync().length).toEqual(1);
+      expect(ndArray.dataSync()[0]).toEqual(1.0);
+    });
+
+    it('should load 4D array correctly', () => {
+      tensor.tensorShape = new tensorflow.TensorShape(
+          {dim: [{size: 1}, {size: 1}, {size: 1}, {size: 1}]});
+
+      const ndArray = tensorToNDArray(tensor);
+      expect(ndArray.rank).toBe(4);
+      expect(ndArray.dtype).toBe('float32');
+      expect(ndArray.dataSync().length).toEqual(1);
+      expect(ndArray.dataSync()[0]).toEqual(1.0);
+    });
   });
 });
