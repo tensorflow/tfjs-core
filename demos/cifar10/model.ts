00247b742eb174dd6c32adec45d051723ab2f201
Yannick Assogba <yassogba@google.com>
2018-01-25 15:59:28 -0500

    use full package import

--- demos/cifar10/model.ts
+++ demos/cifar10/model.ts
@@ -1,5 +1,4 @@
-// tslint:disable-next-line:max-line-length
-import {Array1D, Array2D, Array4D, CheckpointLoader, ENV, NDArray} from 'deeplearn';
+import * as dl from 'deeplearn';
 
 /**
  * Loads model variables from the paths provided in the manifest.
@@ -7,8 +6,8 @@ import {Array1D, Array2D, Array4D, CheckpointLoader, ENV, NDArray} from 'deeplea
  * Returns a promise to an key-value pair of named vars to NDArrays.
  */
 export function loadModelVariables(manifestPath = 'weights/'):
-    Promise<{[varName: string]: NDArray}> {
-  const loader = new CheckpointLoader(manifestPath);
+    Promise<{[varName: string]: dl.NDArray}> {
+  const loader = new dl.CheckpointLoader(manifestPath);
   return loader.getAllVariables();
 }
 
@@ -19,8 +18,8 @@ export function loadModelVariables(manifestPath = 'weights/'):
  * @param input array3D representing image.
  */
 export function inference(
-    modelVars: {[varName: string]: NDArray}, input: Array4D) {
-  const math = ENV.math;
+    modelVars: {[varName: string]: dl.NDArray}, input: dl.Array4D) {
+  const math = dl.ENV.math;
 
   const labelStrings = [
     'airplane',
@@ -38,20 +37,20 @@ export function inference(
   const batchSize = input.shape[0];
 
   // Load the weights and biases from the serialized model
-  const conv1Weights = modelVars['conv1/weights'] as Array4D;
-  const conv1Biases = modelVars['conv1/biases'] as Array1D;
+  const conv1Weights = modelVars['conv1/weights'] as dl.Array4D;
+  const conv1Biases = modelVars['conv1/biases'] as dl.Array1D;
 
-  const conv2Weights = modelVars['conv2/weights'] as Array4D;
-  const conv2Biases = modelVars['conv2/biases'] as Array1D;
+  const conv2Weights = modelVars['conv2/weights'] as dl.Array4D;
+  const conv2Biases = modelVars['conv2/biases'] as dl.Array1D;
 
-  const local3Weights = modelVars['local3/weights'] as Array2D;
-  const local3Biases = modelVars['local3/biases'] as Array1D;
+  const local3Weights = modelVars['local3/weights'] as dl.Array2D;
+  const local3Biases = modelVars['local3/biases'] as dl.Array1D;
 
-  const local4Weights = modelVars['local4/weights'] as Array2D;
-  const local4Biases = modelVars['local4/biases'] as Array1D;
+  const local4Weights = modelVars['local4/weights'] as dl.Array2D;
+  const local4Biases = modelVars['local4/biases'] as dl.Array1D;
 
-  const softmaxWeights = modelVars['softmax_linear/weights'] as Array2D;
-  const softmaxBiases = modelVars['softmax_linear/biases'] as Array1D;
+  const softmaxWeights = modelVars['softmax_linear/weights'] as dl.Array2D;
+  const softmaxBiases = modelVars['softmax_linear/biases'] as dl.Array1D;
 
   // Build the model architecture
 
@@ -71,7 +70,7 @@ export function inference(
         math.localResponseNormalization4D(pool, 4, 1.0, 0.001 / 9.0, 0.75);
 
     // Conv2
-    const conv2Res = math.scope((keep) => {
+    const conv2Res = math.scope(() => {
       const conv = math.conv2d(norm, conv2Weights, conv2Biases, [1, 1], 'same');
       return math.relu(conv);
     });
@@ -82,20 +81,20 @@ export function inference(
     const pool2 = math.maxPool(norm2, [3, 3], [2, 2], 'same');
 
     // Local3
-    const local3Res = math.scope((keep) => {
+    const local3Res = math.scope(() => {
       // Note: -1 in the reshape tells deeplearn to determine the second
       // dimension automatically.
-      const reshaped = math.reshape(pool2, [batchSize, -1]) as Array2D;
-      const mul = math.matMul(reshaped, local3Weights) as Array2D;
+      const reshaped = math.reshape(pool2, [batchSize, -1]) as dl.Array2D;
+      const mul = math.matMul(reshaped, local3Weights) as dl.Array2D;
 
-      const biased = math.add(mul, local3Biases) as Array2D;
+      const biased = math.add(mul, local3Biases) as dl.Array2D;
       return math.relu(biased);
     });
 
     // Local4
-    const local4Res = math.scope((keep) => {
+    const local4Res = math.scope(() => {
       const mul = math.matMul(local3Res, local4Weights);
-      const biased = math.add(mul, local4Biases) as Array2D;
+      const biased = math.add(mul, local4Biases) as dl.Array2D;
       return math.relu(biased);
     });
 
@@ -104,7 +103,7 @@ export function inference(
     // softmax_linear.
     const softmaxRes = math.scope((keep) => {
       const mul = math.matMul(local4Res, softmaxWeights);
-      const biased = math.add(mul, softmaxBiases) as Array2D;
+      const biased = math.add(mul, softmaxBiases) as dl.Array2D;
       const softmax = biased;
 
       return softmax;
@@ -113,8 +112,8 @@ export function inference(
     return softmaxRes;
   });
 
-  const logits = finalRes.reshape([-1, 10]) as Array2D;
-  const prediction = math.argMax(finalRes, 1) as Array1D<'int32'>;
+  const logits = finalRes.reshape([-1, 10]) as dl.Array2D;
+  const prediction = math.argMax(finalRes, 1) as dl.Array1D<'int32'>;
   const predictionLabel =
       Array.from(prediction.dataSync()).map(i => labelStrings[i]);
 
