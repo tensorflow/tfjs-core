3dce783c495ebe237454d3dca02ac5f61abeddd1
Yannick Assogba <yassogba@google.com>
2018-02-23 15:47:27 -0500

    code review fixes

--- demos/mnist_eager/model.ts
+++ demos/mnist_eager/model.ts
@@ -6,6 +6,9 @@ const LEARNING_RATE = .1;
 const BATCH_SIZE = 64;
 const TRAIN_STEPS = 100;
 
+// Data constants.
+const IMAGE_SIZE = 28;
+const LABELS_SIZE = 10;
 const optimizer = dl.train.sgd(LEARNING_RATE);
 
 // Variables that we want to optimize
@@ -16,31 +19,39 @@ const conv1Weights = dl.variable(
 const conv2InputDepth = conv1OutputDepth;
 const conv2OutputDepth = 16;
 const conv2Weights = dl.variable(
-    dl.randomNormal([5, 5, conv2InputDepth, 16], 0, 0.1) as dl.Tensor4D);
+    dl.randomNormal([5, 5, conv2InputDepth, conv2OutputDepth], 0, 0.1) as
+    dl.Tensor4D);
 
 const fullyConnectedWeights = dl.variable(
     dl.randomNormal(
-        [7 * 7 * conv2OutputDepth, 10], 0,
+        [7 * 7 * conv2OutputDepth, LABELS_SIZE], 0,
         1 / Math.sqrt(7 * 7 * conv2OutputDepth)) as dl.Tensor2D);
-const fullyConnectedBias = dl.variable(dl.zeros([10]) as dl.Tensor1D);
+const fullyConnectedBias = dl.variable(dl.zeros([LABELS_SIZE]) as dl.Tensor1D);
 
 // Loss function
 function loss(labels: dl.Tensor2D, ys: dl.Tensor2D) {
   return dl.losses.softmaxCrossEntropy(labels, ys).mean() as dl.Scalar;
 }
 
-// Our model
+// Our actual model
 function model(inputXs: dl.Tensor2D): dl.Tensor2D {
-  const xs = inputXs.as4D(-1, 28, 28, 1);
+  const xs = inputXs.as4D(-1, IMAGE_SIZE, IMAGE_SIZE, 1);
+
+  const strides = 2;
+  const pad = 0;
 
   // Conv 1
   const layer1 = dl.tidy(() => {
-    return xs.conv2d(conv1Weights, 1, 'same').relu().maxPool([2, 2], 2, 0);
+    return xs.conv2d(conv1Weights, 1, 'same')
+        .relu()
+        .maxPool([2, 2], strides, pad);
   });
 
   // Conv 2
   const layer2 = dl.tidy(() => {
-    return layer1.conv2d(conv2Weights, 1, 'same').relu().maxPool([2, 2], 2, 0);
+    return layer1.conv2d(conv2Weights, 1, 'same')
+        .relu()
+        .maxPool([2, 2], strides, pad);
   });
 
   // Final layer
