cd37d674cd808bf755d173d7d2e544cd77d1fdff
Shanqing Cai <cais@google.com>
2018-02-27 23:00:14 -0500

    Add gradients for tile op
    Currently, 1, 2, 3 and 4D cases are covered.

--- src/ops/array_ops.ts
+++ src/ops/array_ops.ts
@@ -672,7 +672,63 @@ export class ArrayOps {
         x.rank === reps.length,
         `Error in transpose: rank of input ${x.rank} ` +
             `must match length of reps ${reps}.`);
-    return ENV.engine.runKernel(backend => backend.tile(x, reps), {x});
+    const grad = (dy: T) => {
+      const derX = () => {
+        let xGrad = ArrayOps.zerosLike(x);
+        if (x.rank === 1) {
+          const steps = dy.shape[0] / x.shape[0];
+          for (let i = 0; i < steps; ++i) {
+            xGrad = xGrad.add(dy.slice([i * x.shape[0]], [x.shape[0]]));
+          }
+        } else if (x.rank === 2) {
+          const steps0 = dy.shape[0] / x.shape[0];
+          const steps1 = dy.shape[1] / x.shape[1];
+          for (let i = 0; i < steps0; ++i) {
+            for (let j = 0; j < steps1; ++j) {
+              xGrad = xGrad.add(dy.slice([i * x.shape[0], j * x.shape[1]],
+                                          [x.shape[0], x.shape[1]]));
+            }
+          }
+        } else if (x.rank === 3) {
+          const steps0 = dy.shape[0] / x.shape[0];
+          const steps1 = dy.shape[1] / x.shape[1];
+          const steps2 = dy.shape[2] / x.shape[2];
+          for (let i = 0; i < steps0; ++i) {
+            for (let j = 0; j < steps1; ++j) {
+              for (let k = 0; k < steps2; ++k) {
+                xGrad = xGrad.add(dy.slice(
+                  [i * x.shape[0], j * x.shape[1], k * x.shape[2]],
+                  [x.shape[0], x.shape[1], x.shape[2]]));
+              }
+            }
+          }
+        } else if (x.rank === 4) {
+          const steps0 = dy.shape[0] / x.shape[0];
+          const steps1 = dy.shape[1] / x.shape[1];
+          const steps2 = dy.shape[2] / x.shape[2];
+          const steps3 = dy.shape[3] / x.shape[3];
+          for (let i = 0; i < steps0; ++i) {
+            for (let j = 0; j < steps1; ++j) {
+              for (let k = 0; k < steps2; ++k) {
+                for (let l = 0; l < steps3; ++l) {
+                  xGrad = xGrad.add(dy.slice(
+                   [i * x.shape[0], j * x.shape[1], k * x.shape[2],
+                    l * x.shape[3]],
+                   [x.shape[0], x.shape[1], x.shape[2], x.shape[3]]));
+                }
+              }
+            }
+         }
+        } else {
+          throw new Error(
+              `Gradient for tile operation is not implemented for rank-` +
+              `${x.rank} tensors yet.`);
+        }
+        return xGrad;
+      };
+      return {x: derX}
+    };
+    return ENV.engine.runKernel(backend => backend.tile(x, reps), {x}, grad);
   }
 
   /**
