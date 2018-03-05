61f87604ccc1c75486ca137a87ad2c72ec881809
Yannick Assogba <yassogba@google.com>
2018-03-05 16:42:03 -0500

    take output path from command line

--- scripts/build-api.ts
+++ scripts/build-api.ts
@@ -1,6 +1,11 @@
 import * as path from 'path';
 import * as shell from 'shelljs';
 
+if (!process.argv[2]) {
+  console.log('Must specify output path');
+  process.exit(1);
+}
+
 const targetScriptPath = './node_modules/deeplearn-docs/src/make-api.ts';
 
 const input = path.resolve('./src/index.ts');
@@ -10,7 +15,7 @@ const repo = path.resolve('./');
 const bundle = path.resolve('./dist/deeplearn.js');
 const github = 'https://github.com/PAIR-code/deeplearnjs';
 
-const out = path.resolve('./api-dist/deeplearn.json');
+const out = path.resolve(process.argv[2]);
 
 // tslint:disable-next-line:max-line-length
 shell.exec(`ts-node ${targetScriptPath} --in ${input} --package ${pkg} --src ${
