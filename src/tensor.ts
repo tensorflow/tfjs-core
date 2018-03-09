59d3456bb8e3fad87d486075700df1e3ad754745
Daniel Smilkov <dsmilkov@gmail.com>
2018-03-09 10:15:18 -0500

    save

--- src/tensor.ts
+++ src/tensor.ts
@@ -177,7 +177,7 @@ export class Tensor<R extends Rank = Rank> {
    */
   readonly strides: number[];
 
-  protected constructor(
+  private constructor(
       shape: ShapeMap[R], dtype: DataType, values?: TypedArray,
       dataId?: DataId) {
     this.size = util.sizeFromShape(shape);
@@ -343,7 +343,7 @@ export class Tensor<R extends Rank = Rank> {
   }
 
   private isDisposed = false;
-  protected throwIfDisposed() {
+  private throwIfDisposed() {
     if (this.isDisposed) {
       throw new Error(`Tensor is disposed.`);
     }
@@ -450,7 +450,7 @@ export class Tensor<R extends Rank = Rank> {
 
   gather<T extends this>(this: T, indices: Tensor1D, axis = 0): T {
     this.throwIfDisposed();
-    return ops.gather(this, indices);
+    return ops.gather(this, indices, axis);
   }
 
   matMul(b: Tensor2D, transposeA = false, transposeB = false): Tensor2D {
