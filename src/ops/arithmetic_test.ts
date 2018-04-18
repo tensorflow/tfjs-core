dd2a9a8e74741b89d18ff5da757b6d6faa006b6f
Nikhil Thorat <nsthorat@google.com>
2018-04-18 11:59:09 -0400

    save

--- src/ops/arithmetic_test.ts
+++ src/ops/arithmetic_test.ts
@@ -29,14 +29,14 @@ describeWithFlags('div', ALL_ENVS, () => {
     expectArraysClose(r, [1, 1, 1, 1, 2.5, 6 / 5]);
   });
 
-  it('division implements floor divide', () => {
-    const a = tf.tensor1d([-5, -4, -3, -3, 3, 3, 2], 'int32');
-    const c = tf.tensor1d([3, 2, -3, 3, 2, 3, 2], 'int32');
+  it('integer division implements floor divide', () => {
+    const a = tf.tensor1d([-6, -6, -5, -4, -3, -3, 3, 3, 2], 'int32');
+    const c = tf.tensor1d([-2, 2, 3, 2, -3, 3, 2, 3, 2], 'int32');
 
     const r = tf.div(a, c);
 
     expect(r.dtype).toEqual('int32');
-    expectArraysClose(r, [-2, -2, 1, 1, 1, 1, 1]);
+    expectArraysClose(r, [3, -3, -2, -2, 1, -1, 1, 1, 1]);
   });
 
   it('integer division broadcasts', () => {
