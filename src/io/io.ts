74b2b6ab8615c328d4688bda7968018bb0004637
Shanqing Cai <cais@google.com>
2018-05-07 17:23:15 -0400

    Add tf.io.browserHTTPRequest
    * Allows model artifacts to be sent via a multipart/form-data
      HTTP request
    
    Towards: https://github.com/tensorflow/tfjs/issues/13

--- src/io/io.ts
+++ src/io/io.ts
@@ -16,6 +16,7 @@
  */
 
 import {browserDownloads, browserFiles} from './browser_files';
+import {browserHTTPRequest} from './browser_http';
 import {browserIndexedDB} from './indexed_db';
 import {decodeWeights, encodeWeights} from './io_utils';
 import {browserLocalStorage} from './local_storage';
@@ -30,6 +31,7 @@ export {
   browserLocalStorage,
   decodeWeights,
   encodeWeights,
+  browserHTTPRequest,
   IOHandler,
   LoadHandler,
   loadWeights,
