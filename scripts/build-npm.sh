bcb2176507f5013d387323077ec6f983f5739b6c
Nikhil Thorat <nsthorat@google.com>
2018-05-15 15:19:08 -0400

    NOT READY: Convert core to use rollup for bundling

--- scripts/build-npm.sh
+++ scripts/build-npm.sh
@@ -16,10 +16,12 @@
 
 set -e
 
-rimraf dist/
+#rimraf dist/
 yarn
 tsc --sourceMap false
-browserify --standalone tfc src/index.ts -p [tsify] > dist/tf-core.js
-uglifyjs dist/tf-core.js -c -m -o dist/tf-core.min.js
+
+#browserify --standalone tfc src/index.ts -p [tsify] > dist/tf-core.js
+#uglifyjs dist/tf-core.js -c -m -o dist/tf-core.min.js
+rollup -c
 echo "Stored standalone library at dist/tf-core(.min).js"
 npm pack
