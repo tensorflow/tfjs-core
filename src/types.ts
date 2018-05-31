3a847e1c6b204298151351f4a3d4c7dc4fa3ca33
Ping Yu <4018+pyu10055@users.noreply.github.com>
2018-05-31 10:00:56 -0700

    add execute method for inference model to allow retrieve activation for any internal nodes

--- src/types.ts
+++ src/types.ts
@@ -138,12 +138,6 @@ export interface ModelPredictConfig {
    * Optional. Verbosity mode. Defaults to false.
    */
   verbose?: boolean;
-
-  /**
-   * Optional. List of output node names to evaluate when running predict().
-   * Defaults to the model's default output.
-   */
-  outputs?: string|string[];
 }
 
 /**
@@ -163,8 +157,7 @@ export interface InferenceModel {
    * If we are provide a batched data of 100 images, the input tensor should be
    * in the shape of [100, 244, 244, 3].
    *
-   * @param config Prediction configuration for specifying the batch size and
-   * output node names.
+   * @param config Prediction configuration for specifying the batch size.
    *
    * @returns Inference result tensors. The output would be single Tensor if
    * model has single output node, otherwise Tensor[] or NamedTensorMap[] will
@@ -172,4 +165,31 @@ export interface InferenceModel {
    */
   predict(inputs: Tensor|Tensor[]|NamedTensorMap, config: ModelPredictConfig):
       Tensor|Tensor[]|NamedTensorMap;
+
+  /**
+   * Execute the inference for the input tensors and return activation values
+   * for specified output node names.
+   *
+   * @param input The input tensors, when there is single input for the model,
+   * inputs param should be a Tensor. For models with mutliple inputs, inputs
+   * params should be in either Tensor[] if the input order is fixed, or
+   * otherwise NamedTensorMap format.
+   * For batch inference execution, the tensors for each input need to be
+   * concatenated together. For example with mobilenet, the required input shape
+   * is [1, 244, 244, 3], which represents the [batch, height, width, channel].
+   * If we are provide a batched data of 100 images, the input tensor should be
+   * in the shape of [100, 244, 244, 3].
+   *
+   * @param config Prediction configuration for specifying the batch size.
+   *
+   * @param outputs string|string[] Optional. List of output node names to
+   * evaluate when running predict(). Defaults to the model's default output.
+   *
+   * @returns Activation values for the output nodes result tensors. The output
+   * would be single Tensor if single output is specified, otherwise Tensor[]
+   * or NamedTensorMap[] for multiple outputs.
+   */
+  execute(
+      inputs: Tensor|Tensor[]|NamedTensorMap, config: ModelPredictConfig,
+      outputs: string|string[]): Tensor|Tensor[]|NamedTensorMap;
 }
