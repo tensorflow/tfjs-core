8f3beac21703e45d180bc11bf8dce1f427f485d3
Daniel Smilkov <dsmilkov@gmail.com>
2018-06-02 13:28:03 -0400

    save

--- karma.conf.js
+++ karma.conf.js
@@ -40,14 +40,14 @@ module.exports = function(config) {
         browser: 'chrome',
         browser_version: 'latest',
         os: 'OS X',
-        os_version: 'Sierra'
+        os_version: 'High Sierra'
       },
       bs_firefox_mac: {
         base: 'BrowserStack',
         browser: 'firefox',
         browser_version: 'latest',
         os: 'OS X',
-        os_version: 'Sierra'
+        os_version: 'High Sierra'
       },
       chrome_with_swift_shader: {
         base: 'Chrome',
