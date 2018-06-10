32d0258c483b4279c5c9a18b835db0523edfc8e3
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2018-06-10 22:11:30 +0530

    Array Ops: Fixes Unstack ops documentation example

--- src/ops/array_ops.ts
+++ src/ops/array_ops.ts
@@ -1136,7 +1136,10 @@ export class ArrayOps {
    *
    * ```js
    * const a = tf.tensor2d([1, 2, 3, 4], [2, 2]);
-   * tf.unstack(a).print();
+   *
+   * tf.unstack(a).forEach(function(tensor) {
+   *   tensor.print();
+   * });
    * ```
    *
    * @param value A tensor object.
