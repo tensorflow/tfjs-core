0fbbef6b7201caf885ebb52cde128b7420d7665c
Nikhil Thorat <nsthorat@google.com>
2018-06-12 09:43:00 -0400

    80 chars

--- src/kernels/webgl/encode_float_gpu.ts
+++ src/kernels/webgl/encode_float_gpu.ts
@@ -68,16 +68,19 @@ export class EncodeFloatProgram implements GPGPUProgram {
 
         // TODO: correctly handle denormal numbers
         // http://www.2ality.com/2012/04/number-encoding.html
-        float a = abs(val);                           // encode absolute value + sign
+        // encode absolute value + sign
+        float a = abs(val);
         float exp = floor(log2(a));                 // number of powers of 2
-        float mant = pow(2.,log2(a)-exp) * pow(2.,23.);  // multiply to fill 24 bits (implied leading 1)
+        // multiply to fill 24 bits (implied leading 1)
+        float mant = pow(2.,log2(a)-exp) * pow(2.,23.);
         float mant1 = floor(mant / 256. / 256.);    // first 8 bits of mantissa
         float mant2 = mod(floor(mant / 256.),256.); // second 8 bits
         float mant3 = mod(mant,256.);               // third 8 bits
 
         highp float sign = 128.-128.*(a/val);			// sign bit is 256 or 0
         highp float e = (sign+exp+127.)/510.;		// exponent and sign
-        highp float m1 = (mant1-(128.*(1.-mod(exp+127.,2.))))/255.; // handle leading bit
+        // handle leading bit
+        highp float m1 = (mant1-(128.*(1.-mod(exp+127.,2.))))/255.;
         highp float m2 = (mant2)/255.;				// middle part
         highp float m3 = (mant3+.5)/255.;			// scale to 0 - 255
 
