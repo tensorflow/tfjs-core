c12b2e8619db80f33842d151220598ff524e85e4
Nikhil Thorat <nsthorat@google.com>
2018-06-12 09:33:33 -0400

    try new encoder

--- src/kernels/webgl/encode_float_gpu.ts
+++ src/kernels/webgl/encode_float_gpu.ts
@@ -64,9 +64,29 @@ export class EncodeFloatProgram implements GPGPUProgram {
         return c / 255.0;
       }
 
+      vec4 encode_float2(float val) {
+
+        // TODO: correctly handle denormal numbers
+        // http://www.2ality.com/2012/04/number-encoding.html
+        float a = abs(val);                           // encode absolute value + sign
+        float exp = floor(log2(a));                 // number of powers of 2
+        float mant = pow(2.,log2(a)-exp) * pow(2.,23.);  // multiply to fill 24 bits (implied leading 1)
+        float mant1 = floor(mant / 256. / 256.);    // first 8 bits of mantissa
+        float mant2 = mod(floor(mant / 256.),256.); // second 8 bits
+        float mant3 = mod(mant,256.);               // third 8 bits
+
+        highp float sign = 128.-128.*(a/val);			// sign bit is 256 or 0
+        highp float e = (sign+exp+127.)/510.;		// exponent and sign
+        highp float m1 = (mant1-(128.*(1.-mod(exp+127.,2.))))/255.; // handle leading bit
+        highp float m2 = (mant2)/255.;				// middle part
+        highp float m3 = (mant3+.5)/255.;			// scale to 0 - 255
+
+        return vec4(m3,m2,m1,e);
+      }
+
       void main() {
         float x = getAAtOutCoords();
-        gl_FragColor = encode_float(x);
+        gl_FragColor = encode_float2(x);
       }
     `;
   }
