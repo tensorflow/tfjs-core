1e7aa58f1aa0cd6b9d653ac0324638f0473d2c91
Nikhil Thorat <nsthorat@google.com>
2018-06-13 10:37:47 -0400

    softmax overflow

--- src/kernels/webgl/shader_compiler.ts
+++ src/kernels/webgl/shader_compiler.ts
@@ -199,8 +199,7 @@ const SHADER_PREFIX = `
    // float v1 = val * val;
    // float v2 = val * val;
    // return v1 == v2 ? false : true;
-    return ( val < 0.0 || 0.0 < val || val == 0.0 ) ? false : true;
-
+    return (val < 0.0 || 0.0 < val || val == 0.0) ? false : true;
   }
 
   bool hasNaN(vec4 values) {
