4a1cf20e076b2ed824c738d383fc5c37501cb362
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2018-06-18 22:42:31 +0530

    Optimizers: Remove debug statements

--- src/optimizers/adadelta_optimizer.ts
+++ src/optimizers/adadelta_optimizer.ts
@@ -50,10 +50,7 @@ export class AdadeltaOptimizer extends Optimizer {
       epsilon = optimizer_utils.getOptimizerDefaultEpsilonValue();
     }
 
-    console.log(epsilon);
-
     this.epsilonScalar = keep(scalar(epsilon));
-    this.epsilonScalar.print();
   }
 
   applyGradients(variableGradients: NamedVariableMap) {
@@ -82,11 +79,6 @@ export class AdadeltaOptimizer extends Optimizer {
         const newAccumulatedGrad =
             this.rhoScalar.mul(accumulatedGrad)
                 .add(this.oneMinusRho.mul(gradient.square()));
-
-        this.epsilonScalar.print();
-        accumulatedGrad.print();
-        accumulatedGrad.add(this.epsilonScalar).print();
-        accumulatedGrad.add(this.epsilonScalar).sqrt().print();
 
         const updates = accumulatedUpdate.add(this.epsilonScalar)
                             .sqrt()
