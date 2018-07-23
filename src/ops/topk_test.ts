a40074e22fdee8be3dec00e1e3e0a3311da0889f
Daniel Smilkov <dsmilkov@gmail.com>
2018-07-23 16:44:25 -0400

    save

--- src/ops/topk_test.ts
+++ src/ops/topk_test.ts
@@ -130,30 +130,3 @@ describeWithFlags('topk', ALL_ENVS, () => {
     expectArraysClose(indices, [2, 3]);
   });
 });
-
-describeWithFlags('topkAsync', ALL_ENVS, () => {
-  it('1d array with default k', async () => {
-    const a = tensor1d([20, 10, 40, 30]);
-    const {values, indices} = await tf.topkAsync(a);
-
-    expect(values.shape).toEqual([1]);
-    expect(indices.shape).toEqual([1]);
-    expect(values.dtype).toBe('float32');
-    expect(indices.dtype).toBe('int32');
-    expectArraysClose(values, [40]);
-    expectArraysClose(indices, [2]);
-  });
-
-  it('accepts a tensor-like object, k=2', async () => {
-    const a = [20, 10, 40, 30];
-    const k = 2;
-    const {values, indices} = await tf.topkAsync(a, k);
-
-    expect(values.shape).toEqual([2]);
-    expect(indices.shape).toEqual([2]);
-    expect(values.dtype).toBe('float32');
-    expect(indices.dtype).toBe('int32');
-    expectArraysClose(values, [40, 30]);
-    expectArraysClose(indices, [2, 3]);
-  });
-});
