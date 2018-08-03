b1d8f72edce7d594d5deaf1637668211cd92643a
Zachary Boldyga <zach@scalabull.com>
2018-08-03 14:49:01 -0700

    unit tests cleaned up and more tests added. Located a bug in one test case

--- src/ops/conv3d_test.ts
+++ src/ops/conv3d_test.ts
@@ -20,36 +20,61 @@ import {describeWithFlags} from '../jasmine_util';
 // tslint:disable-next-line:max-line-length
 import {ALL_ENVS, expectArraysClose} from '../test_util';
 
+function get5DTensorArea(tensor: [number, number, number, number, number]) {
+  return tensor[0] * tensor[1] * tensor[2] * tensor[3] * tensor[4];
+}
+
+// Generates small floating point inputs to avoid overflows
+function generateCaseInputs(totalSizeTensor: number, totalSizeFilter: number) {
+  const inp = new Array(totalSizeTensor);
+  const filt = new Array(totalSizeFilter);
+
+  for (let i = 0; i < totalSizeTensor; i++) {
+    inp[i] = 1.0 * (i + 1) / totalSizeTensor;
+  }
+  for (let i = 0; i < totalSizeFilter; i++) {
+    filt[i] = 1.0 * (i + 1) / totalSizeFilter;
+  }
+
+  return {input: inp, filter: filt};
+}
+
+// TODO: add case insensitivity for pad string value?
+function runConv3DTestCase(
+    batch: number, inDepth: number, inHeight: number, inWidth: number,
+    inChannels: number, outChannels: number, fDepth: number, fHeight: number,
+    fWidth: number, pad: 'valid'|'same',
+    stride: [number, number, number]|number) {
+  const inputShape: [number, number, number, number, number] =
+      [batch, inDepth, inHeight, inWidth, inChannels];
+  const filterShape: [number, number, number, number, number] =
+      [fDepth, fHeight, fWidth, inChannels, outChannels];
+
+  const totalSizeTensor = get5DTensorArea(inputShape);
+  const totalSizeFilter = get5DTensorArea(filterShape);
+  const inputs = generateCaseInputs(totalSizeTensor, totalSizeFilter);
+
+  const x = tf.tensor5d(inputs.input, inputShape);
+  const w = tf.tensor5d(inputs.filter, filterShape);
+
+  const result = tf.conv3d(x, w, stride, pad);
+  return result;
+}
+
 describeWithFlags('conv3d', ALL_ENVS, () => {
-  it('x=[1, 2, 3, 1, 3] f=[1, 1, 1, 3, 3] s=1 d=1 p=0', () => {
+  it('x=[1, 2, 3, 1, 3] f=[1, 1, 1, 3, 3] s=1 d=1 p=valid', () => {
     const batch = 1;
-    const inputDepth = 2;
+    const inDepth = 2;
+    const inHeight = 3;
+    const inWidth = 1;
     const inChannels = 3;
     const outChannels = 3;
-    const inputShape: [number, number, number, number, number] =
-        [batch, inputDepth, 3, 1, inChannels];
     const fSize = 1;
-    // TODO: add case insensitivity for pad string value?
     const pad = 'valid';
     const stride = 1;
-
-    // Inputs are small to avoid overflows
-    const totalSizeTensor = batch * inputDepth * inChannels * outChannels;
-    const totalSizeFilter = inChannels * outChannels;
-    const inp = new Array(totalSizeTensor);
-    const filt = new Array(totalSizeFilter);
-
-    for (let i = 0; i < totalSizeTensor; i++) {
-      inp[i] = 1.0 * (i + 1) / totalSizeTensor;
-    }
-    for (let i = 0; i < totalSizeFilter; i++) {
-      filt[i] = 1.0 * (i + 1) / totalSizeFilter;
-    }
-
-    const x = tf.tensor5d(inp, inputShape);
-    const w = tf.tensor5d(filt, [fSize, fSize, fSize, inChannels, outChannels]);
-
-    const result = tf.conv3d(x, w, stride, pad);
+    const result = runConv3DTestCase(
+        batch, inDepth, inHeight, inWidth, inChannels, outChannels, fSize,
+        fSize, fSize, pad, stride);
 
     const expectedOutput = [
       0.18518519, 0.22222222, 0.25925926, 0.40740741, 0.5, 0.59259259,
@@ -59,59 +84,181 @@ describeWithFlags('conv3d', ALL_ENVS, () => {
 
     expectArraysClose(result, expectedOutput);
   });
-  /*
-    it('x=[1, 2, 1, 3, 3] f=[1, 1, 1, 3, 3] s=1 d=1 p=0', () => {
-      const batch = 1;
-      const inputDepth = 2;
-      const inChannels = 3;
-      const outChannels = 3;
-      const inputShape: [number, number, number, number, number] =
-          [batch, inputDepth, 1, 3, inChannels];
-      const fSize = 1;
-      const pad = 0;
-      const stride = 1;
-      const inp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
-    18]; const filt = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  it('x=[1, 2, 1, 3, 3] f=[1, 1, 1, 3, 3] s=1 d=1 p=valid', () => {
+    const batch = 1;
+    const inDepth = 2;
+    const inHeight = 1;
+    const inWidth = 3;
+    const inChannels = 3;
+    const outChannels = 3;
+    const fSize = 1;
+    const pad = 'valid';
+    const stride = 1;
+    const result = runConv3DTestCase(
+        batch, inDepth, inHeight, inWidth, inChannels, outChannels, fSize,
+        fSize, fSize, pad, stride);
 
-      const x = tf.tensor5d(inp, inputShape);
-      const w = tf.tensor5d(filt, [fSize, fSize, fSize, inChannels,
-    outChannels]);
+    const expectedOutput = [
+      0.18518519, 0.22222222, 0.25925926, 0.40740741, 0.5, 0.59259259,
+      0.62962963, 0.77777778, 0.92592593, 0.85185185, 1.05555556, 1.25925926,
+      1.07407407, 1.33333333, 1.59259259, 1.2962963, 1.61111111, 1.92592593
+    ];
 
-      const result = tf.conv3d(x, w, stride, pad);
-      const expectedOutput = [
-        0.18518519, 0.22222222, 0.25925926, 0.40740741, 0.5, 0.59259259,
-        0.62962963, 0.77777778, 0.92592593, 0.85185185, 1.05555556, 1.25925926,
-        1.07407407, 1.33333333, 1.59259259, 1.2962963, 1.61111111, 1.92592593
-      ];
+    expectArraysClose(result, expectedOutput);
+  });
 
-      expectArraysClose(result, expectedOutput);
-    });
+  it('x=[1, 1, 2, 3, 3] f=[1, 1, 1, 3, 3] s=1 d=1 p=valid', () => {
+    const batch = 1;
+    const inDepth = 1;
+    const inHeight = 2;
+    const inWidth = 3;
+    const inChannels = 3;
+    const outChannels = 3;
+    const fSize = 1;
+    const pad = 'valid';
+    const stride = 1;
+    const result = runConv3DTestCase(
+        batch, inDepth, inHeight, inWidth, inChannels, outChannels, fSize,
+        fSize, fSize, pad, stride);
 
-    it('x=[1, 1, 2, 3, 3] f=[1, 1, 1, 3, 3] s=1 d=1 p=0', () => {
-      const batch = 1;
-      const inputDepth = 1;
-      const inChannels = 3;
-      const outChannels = 3;
-      const inputShape: [number, number, number, number, number] =
-          [batch, inputDepth, 2, 3, inChannels];
-      const fSize = 1;
-      const pad = 0;
-      const stride = 1;
-      const inp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
-    18]; const filt = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const expectedOutput = [
+      0.18518519, 0.22222222, 0.25925926, 0.40740741, 0.5, 0.59259259,
+      0.62962963, 0.77777778, 0.92592593, 0.85185185, 1.05555556, 1.25925926,
+      1.07407407, 1.33333333, 1.59259259, 1.2962963, 1.61111111, 1.92592593
+    ];
 
-      const x = tf.tensor5d(inp, inputShape);
-      const w = tf.tensor5d(filt, [fSize, fSize, fSize, inChannels,
-    outChannels]);
+    expectArraysClose(result, expectedOutput);
+  });
 
-      const result = tf.conv3d(x, w, stride, pad);
-      const expectedOutput = [
-        0.18518519, 0.22222222, 0.25925926, 0.40740741, 0.5, 0.59259259,
-        0.62962963, 0.77777778, 0.92592593, 0.85185185, 1.05555556, 1.25925926,
-        1.07407407, 1.33333333, 1.59259259, 1.2962963, 1.61111111, 1.92592593
-      ];
+  it('x=[1, 4, 2, 3, 3] f=[2, 2, 2, 3, 3] s=1 d=1 p=valid', () => {
+    const batch = 1;
+    const inDepth = 4;
+    const inHeight = 2;
+    const inWidth = 3;
+    const inChannels = 3;
+    const outChannels = 3;
+    const fSize = 2;
+    const pad = 'valid';
+    const stride = 1;
+    const result = runConv3DTestCase(
+        batch, inDepth, inHeight, inWidth, inChannels, outChannels, fSize,
+        fSize, fSize, pad, stride);
 
-      expectArraysClose(result, expectedOutput);
-    });
-    */
+    const expectedOutput = [
+      3.77199074, 3.85069444, 3.92939815, 4.2650463, 4.35763889, 4.45023148,
+      6.73032407, 6.89236111, 7.05439815, 7.22337963, 7.39930556, 7.57523148,
+      9.68865741, 9.93402778, 10.17939815, 10.18171296, 10.44097222, 10.70023148
+    ];
+
+    expectArraysClose(result, expectedOutput);
+  });
+
+  // TODO: this case currently fails. NOTE: python test case seems to have less
+  // decimal precision, float16 type is used in their tests
+  // Seems this could be an issue with multi-dimensional stride inputs
+  it('x=[1, 5, 8, 7, 1] f=[1, 2, 3, 1, 1] s=[2, 3, 1] d=1 p=same', () => {
+    const batch = 1;
+    const inDepth = 5;
+    const inHeight = 8;
+    const inWidth = 7;
+    const inChannels = 1;
+    const outChannels = 1;
+    const fDepth = 1;
+    const fHeight = 2;
+    const fWidth = 3;
+    const pad = 'same';
+    const stride: [number, number, number] = [2, 3, 1];
+    const result = runConv3DTestCase(
+        batch, inDepth, inHeight, inWidth, inChannels, outChannels, fDepth,
+        fHeight, fWidth, pad, stride);
+
+    const expectedOutput = [
+      0.06071429, 0.08988095, 0.10238095, 0.11488095, 0.12738095, 0.13988095,
+      0.08452381, 0.26071429, 0.35238095, 0.36488095, 0.37738095, 0.38988095,
+      0.40238095, 0.23452381, 0.46071429, 0.61488095, 0.62738095, 0.63988095,
+      0.65238095, 0.66488095, 0.38452381, 1.12738095, 1.48988095, 1.50238095,
+      1.51488095, 1.52738095, 1.53988095, 0.88452381, 1.32738095, 1.75238095,
+      1.76488095, 1.77738095, 1.78988095, 1.80238095, 1.03452381, 1.52738095,
+      2.01488095, 2.02738095, 2.03988095, 2.05238095, 2.06488095, 1.18452381,
+      2.19404762, 2.88988095, 2.90238095, 2.91488095, 2.92738095, 2.93988095,
+      1.68452381, 2.39404762, 3.15238095, 3.16488095, 3.17738095, 3.18988095,
+      3.20238095, 1.83452381, 2.59404762, 3.41488095, 3.42738095, 3.43988095,
+      3.45238095, 3.46488095, 1.98452381
+    ];
+
+    expectArraysClose(result, expectedOutput);
+  });
+
+  it('x=[1, 4, 2, 3, 3] f=[2, 2, 2, 3, 3] s=2 d=1 p=valid', () => {
+    const batch = 1;
+    const inDepth = 4;
+    const inHeight = 2;
+    const inWidth = 3;
+    const inChannels = 3;
+    const outChannels = 3;
+    const fSize = 2;
+    const pad = 'valid';
+    const stride = 2;
+    const result = runConv3DTestCase(
+        batch, inDepth, inHeight, inWidth, inChannels, outChannels, fSize,
+        fSize, fSize, pad, stride);
+
+    const expectedOutput = [
+      3.77199074, 3.85069444, 3.92939815, 9.68865741, 9.93402778, 10.17939815
+    ];
+
+    expectArraysClose(result, expectedOutput);
+  });
+
+  it('x=[1, 6, 7, 8, 2] f=[3, 2, 1, 2, 3] s=3 d=1 p=valid', () => {
+    const batch = 1;
+    const inDepth = 6;
+    const inHeight = 7;
+    const inWidth = 8;
+    const inChannels = 2;
+    const outChannels = 3;
+    const fDepth = 3;
+    const fHeight = 2;
+    const fWidth = 1;
+    const pad = 'valid';
+    const stride = 3;
+    const result = runConv3DTestCase(
+        batch, inDepth, inHeight, inWidth, inChannels, outChannels, fDepth,
+        fHeight, fWidth, pad, stride);
+
+    const expectedOutput = [
+      1.51140873, 1.57167659, 1.63194444, 1.56349206, 1.62673611, 1.68998016,
+      1.6155754,  1.68179563, 1.74801587, 1.9280754,  2.01215278, 2.09623016,
+      1.98015873, 2.0672123,  2.15426587, 2.03224206, 2.12227183, 2.21230159,
+      4.4280754,  4.65500992, 4.88194444, 4.48015873, 4.71006944, 4.93998016,
+      4.53224206, 4.76512897, 4.99801587, 4.84474206, 5.09548611, 5.34623016,
+      4.8968254,  5.15054563, 5.40426587, 4.94890873, 5.20560516, 5.46230159
+    ];
+
+    expectArraysClose(result, expectedOutput);
+  });
+
+  it('x=[1, 4, 2, 3, 3] f=[2, 2, 2, 3, 3] s=2 d=1 p=same', () => {
+    const batch = 1;
+    const inDepth = 4;
+    const inHeight = 2;
+    const inWidth = 3;
+    const inChannels = 3;
+    const outChannels = 3;
+    const fSize = 2;
+    const pad = 'same';
+    const stride = 2;
+    const result = runConv3DTestCase(
+        batch, inDepth, inHeight, inWidth, inChannels, outChannels, fSize,
+        fSize, fSize, pad, stride);
+
+    const expectedOutput = [
+      3.77199074, 3.85069444, 3.92939815, 2.0162037, 2.06597222, 2.11574074,
+      9.68865741, 9.93402778, 10.17939815, 4.59953704, 4.73263889, 4.86574074
+    ];
+
+    expectArraysClose(result, expectedOutput);
+  });
+
+  // NOTE: stopped @ 'testKernelSmallerThanStride'
 });
