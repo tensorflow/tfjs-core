441c2f98b73b30aa52aa3141cb1dbb34949cdf7e
AviKndr <avikndr@gmail.com>
2018-08-23 18:28:08 -0700

    partial implementation of backpropImage kernel on gpuBackend

--- src/kernels/webgl/crop_and_resize_backprop_image_gpu.ts
+++ src/kernels/webgl/crop_and_resize_backprop_image_gpu.ts
@@ -28,6 +28,15 @@ export class CropAndResizeBackpropImageProgram implements GPGPUProgram {
     this.outputShape = [batch, imageHeight, imageWidth, depth];
     const methodId = method == 'bilinear' ? 1 : 0;
 
+    const heightScale = (cropHeight > 1) ? (imageHeight - 1) / (cropHeight - 1) : 0
+    const widthScale = (cropWidth > 1) ? (imageWidth - 1) / (cropWidth - 1) : 0
+    const invHeightScale = (cropHeight > 1) ? (cropHeight - 1) / (imageHeight - 1) : 0
+    const invWidthScale = (cropWidth > 1) ? (cropWidth - 1) / (imageWidth - 1) : 0
+    // This defines the size of the window of values around a particular
+    // index in dy that we want to search for contributions to dx.
+    const winHeight = (Math.ceil(invHeightScale) * 2) + 2;
+    const winWidth = (Math.ceil(invWidthScale) * 2) + 2;
+
     this.userCode = `
     void main() {
       ivec4 coords = getOutputCoords();
@@ -37,6 +46,16 @@ export class CropAndResizeBackpropImageProgram implements GPGPUProgram {
       int d = coords[3];
 
       float accumulator = 0.0;
+      float height_scale = (${cropHeight} > 1)
+      ? (y2-y1) * ${imageHeight - 1}/${cropHeight - 1}
+      : 0;
+      float width_scale = (${cropWidth} > 1)
+      ? (y2-y1) * ${imageWidth - 1}/${cropWidth - 1}
+      : 0;
+      float invHeightScale = float(${invHeightScale});
+      float invWidthScale = float(${invWidthScale});
+      int winHeight = int(${winHeight});
+      int winWidth = int(${winWidth});
 
       for(int b = 0; b < ${numBoxes}; b++) {
         int bInd = getBoxInd(b);
@@ -49,57 +68,73 @@ export class CropAndResizeBackpropImageProgram implements GPGPUProgram {
         int y2 = getBoxes(b,2);
         int x2 = getBoxes(b,3);
 
-        float height_scale = (${cropHeight} > 1)
-        ? (y2-y1) * ${imageHeight - 1}/${cropHeight - 1}
-        : 0;
-        float width_scale = (${cropWidth} > 1)
-        ? (y2-y1) * ${imageWidth - 1}/${cropWidth - 1}
-        : 0;
+        // loop over dy for points in image that may be topLeft, topRight, ...
+        // convert from image to crop coords and for loop through window
 
-        float in_y = (${cropHeight} > 1)
-        ? y1 * (${imageHeight - 1} + y*(height_scale)
-        : 0.5 * (y1+y2) * ${imageHeight - 1};
+        // Compute bounds for where in dy we will look
+        float startRLerp = floor(float(r) * invHeightScale);
+        int startDyR = int(startRLerp - float(winHeight / 2));
 
-        if( in_y < 0 || in_y > ${imageHeight - 1} ) {
-          continue
+        float startCLerp = floor(float(c) * invWidthScale);
+        int startDyC = int(startCLerp - float(winWidth / 2));
+
+        // Loop over dy
+        for (int dyROffset = 0; dyROffset < winHeight; dyROffset++) {
+          int dyR = dyROffset + startDyR;
+
+          // Guard against the window exceeding the bounds of dy
+          if (dyR < 0 || dyR >= ${cropHeight}) {
+            continue;
+          }
+
+          for (int dyCOffset = 0; dyCOffset < winWidth; dyCOffset++) {
+            int dyC = dyCOffset + startDyC;
+
+            // Guard against the window exceeding the bounds of dy
+            if (dyC < 0 || dyC >= ${cropWidth}) {
+              continue;
+            }
+
+            float in_y = float(dyR) * ${heightScale};
+            float in_x = float(dyC) * ${widthScale};
+            if( in_y < 0 || in_y > ${imageHeight - 1} ||
+                in_x < 0 || in_x > ${imageWidth - 1} ) {
+              continue;
+            }
+
+            // Fractional source index.
+            vec2 sourceFracIndexRC = vec2(float(dyR) * ${heightScale},float(dyC) * ${widthScale})
+            ivec2 sourceFloorRC = ivec2(sourceFracIndexRC);
+            ivec2 sourceCeilRC = ivec2(ceil(sourceFracIndexRC));
+            vec2 fracRC = sourceFracIndexRC - vec2(sourceFloorRC);
+            vec2 invFracRC = vec2(1.0,1.0) - fracRC;
+
+            if (y == sourceFloorRC[0] && x == sourceFloorRC[1]) {
+              // topLeft
+              accumulator +=
+                getDy(b, dyR, dyC, d) * inverseDxRLerp * inverseDxCLerp;
+            }
+
+            if (r == topDxRIndex && c == rightDxCIndex) {
+              // topRight
+              accumulator += getDy(b, dyR, dyC, d) * inverseDxRLerp * dxCLerp;
+            }
+
+            if (r == bottomDxRIndex && c == leftDxCIndex) {
+              // bottomLeft
+              accumulator += getDy(b, dyR, dyC, d) * dxRLerp * inverseDxCLerp;
+            }
+
+            if (r == bottomDxRIndex && c == rightDxCIndex) {
+              // bottomRight
+              accumulator += getDy(b, dyR, dyC, d) * dxRLerp * dxCLerp;
+            }
+          }
         }
-
-        float in_x = (${cropWidth} > 1)
-        ? x1 * (${imageWidth - 1} + x*(width_scale)
-        : 0.5 * (x1+x2) * ${imageWidth - 1};
-        if( in_x < 0 || in_x > ${imageWidth - 1} ) {
-          continue
-        }
-
-        // Fractional source index.
-        vec2 sourceFracIndexRC = vec2(in_y,in_x)
-
-        if(${methodId} == 1) {
-
-          // Compute the four integer indices.
-          ivec2 sourceFloorRC = ivec2(sourceFracIndexRC);
-          ivec2 sourceCeilRC = ivec2(ceil(sourceFracIndexRC));
-          vec2 fracRC = sourceFracIndexRC - vec2(sourceFloorRC);
-
-          float dtop = (1-fracRC[0]) * getGrad()
-
-          float topLeft = getA(b, sourceFloorRC.x, sourceFloorRC.y, d);
-          float bottomLeft = getA(b, sourceCeilRC.x, sourceFloorRC.y, d);
-          float topRight = getA(b, sourceFloorRC.x, sourceCeilRC.y, d);
-          float bottomRight = getA(b, sourceCeilRC.x, sourceCeilRC.y, d);
-
-
-          float top = topLeft + (topRight - topLeft) * fracRC.y;
-          float bottom = bottomLeft + (bottomRight - bottomLeft) * fracRC.y;
-          float newValue = top + (bottom - top) * fracRC.x;
-          setOutput(newValue);
-        } else {
-          // Compute the coordinators of nearest neighbor point.
-          ivec2 sourceNearestRC = ivec2(floor(sourceFracIndexRC + 0.5));
-          float newValue = getA(b, sourceNearestRC.x, sourceNearestRC.y, d);
-          setOutput(newValue);
-        }
+        // End loop over region of box in dy
       }
+      // End loop over box in dy
+      setOutput(accumulator);
     }
   `;
   }
