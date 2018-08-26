8125e9ab14b43abf43f2cee63f8a3f5cc4532e4b
Ann Yuan <annyuan@google.com>
2018-08-26 18:04:39 -0400

    fix pack

--- src/kernels/webgl/pack_gpu.ts
+++ src/kernels/webgl/pack_gpu.ts
@@ -11,12 +11,13 @@ export class PackProgram implements GPGPUProgram {
 		this.userCode = `
 			void main() {
 				vec2 onePixel = vec2(1.) / vec2(${inputShape[1]}, ${inputShape[0]});
+				vec2 coord = ((gl_FragCoord.xy - halfCR) * 2.) / vec2(${inputShape[1]}, ${inputShape[0]});
 
 				gl_FragColor = vec4(
-					sampleTexture(A, resultUV), 
-					sampleTexture(A, vec2(resultUV.x + onePixel.x, resultUV.y)), 
-					sampleTexture(A, vec2(resultUV.x, resultUV.y + onePixel.y)), 
-					sampleTexture(A, resultUV + onePixel));
+					sampleTexture(A, coord), 
+					sampleTexture(A, vec2(coord.x + onePixel.x, coord.y)), 
+					sampleTexture(A, vec2(coord.x, coord.y + onePixel.y)), 
+					sampleTexture(A, coord + onePixel));
 			}
 		`;
 	}
