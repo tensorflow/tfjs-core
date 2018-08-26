f4700df265a830d0b3f85f3c8b43a3959847ddb7
Ann Yuan <annyuan@google.com>
2018-08-26 14:11:27 -0400

    unpacking

---
+++ src/kernels/webgl/unpack_gpu.ts
@@ -0,0 +1,34 @@
+import {GPGPUProgram} from './gpgpu_math';
+
+export class UnpackProgram implements GPGPUProgram {
+	variableNames = ['A'];
+	userCode: string;
+	outputShape: number[];
+
+	constructor(outputShape: number[]) {
+		this.outputShape = outputShape;
+		this.userCode = `
+			float eps = 0.0001;
+
+			void main() {
+				vec4 packedInput = texture2D(A, resultUV / 2.);
+
+				vec2 coord = floor(gl_FragCoord.xy);
+
+				if(mod(coord.x, 2.0) < eps) { // x is even
+					if(mod(coord.y, 2.0) < eps) { // y is even
+						gl_FragColor = vec4(packedInput.r, 0, 0, 0);
+					} else {
+						gl_FragColor = vec4(packedInput.b, 0, 0, 0);
+					}
+				} else { // x is odd
+					if(mod(coord.y, 2.0) < eps) {
+						gl_FragColor = vec4(packedInput.g, 0, 0, 0);
+					} else {
+						gl_FragColor = vec4(packedInput.a, 0, 0, 0);
+					}
+				}
+			}
+		`;
+	}
+}
