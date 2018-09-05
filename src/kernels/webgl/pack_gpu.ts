1acd378bb0f12cedaf949c8c7d2918ead8ba441e
Ann Yuan <annyuan@google.com>
2018-09-05 16:19:42 -0400

    edit packing

--- src/kernels/webgl/pack_gpu.ts
+++ src/kernels/webgl/pack_gpu.ts
@@ -13,14 +13,17 @@ export class PackProgram implements GPGPUProgram {
       void main() {
         ivec2 rc = getOutputCoords();
 
+        int r = rc.x * 2;
+        int c = rc.y * 2;
+        int rp1 = r + 1;
+        int cp1 = c + 1;
+
         gl_FragColor = vec4(
-            getA(rc.x * 2, rc.y * 2),
-            rc.y * 2 + 1 >= ${
-        outputShape[1]} ? 0. : getA(rc.x * 2, rc.y * 2 + 1),
-            rc.x * 2 + 1 >= ${
-        outputShape[0]} ? 0. : getA(rc.x * 2 + 1, rc.y * 2),
-            rc.x * 2 + 1 >= ${outputShape[0]} || rc.y * 2 + 1 > ${
-        outputShape[1]} ? 0. : getA(rc.x * 2 + 1, rc.y * 2 + 1)
+            getA(r, c),
+            cp1 >= ${outputShape[1]} ? 0. : getA(r, cp1),
+            rp1 >= ${outputShape[0]} ? 0. : getA(rp1, c),
+            rp1 >= ${outputShape[0]} || cp1 >= ${
+        outputShape[1]} ? 0. : getA(rp1, cp1)
           );
       }
     `;
