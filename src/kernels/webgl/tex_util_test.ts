7c1cb716ed177924f852edc40cee21c48ee57eb2
Ann Yuan <annyuan@google.com>
2018-10-17 14:38:07 -0400

    semicolon

--- src/kernels/webgl/tex_util_test.ts
+++ src/kernels/webgl/tex_util_test.ts
@@ -395,7 +395,7 @@ describeWithFlags('tex_util decodeMatrixFromPackedRGBA', WEBGL_ENVS, () => {
     expectArraysClose(
         matrix,
         new Float32Array(
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]))
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]));
   });
 
   it('4D (2x3x3x4) is properly decoded', () => {
