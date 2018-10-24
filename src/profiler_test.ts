0910133a368446286dc163d3f46a6ce1a0c3ce81
Ann Yuan <annyuan@google.com>
2018-10-24 09:13:51 -0400

    create separate webgl timing profiler test

--- src/profiler_test.ts
+++ src/profiler_test.ts
@@ -15,14 +15,116 @@
  * =============================================================================
  */
 
+import {describeWithFlags} from '../jasmine_util';
+import {WEBGL_ENVS} from '../test_util';
+
 import * as tf from './index';
 import {BackendTimer, BackendTimingInfo} from './kernels/backend';
-import {GPUProgramsInfo} from './kernels/backend_webgl';
+import {GPUProgramsInfo, WebGLTimingInfo} from './kernels/backend_webgl';
 import {TypedArray} from './kernels/webgl/tex_util';
 import {Logger, Profiler} from './profiler';
 import {Tensor} from './tensor';
 
 class TestBackendTimer implements BackendTimer {
+  private counter = 1;
+  constructor(private delayMs: number, private queryTimeMs: number) {}
+
+  async time(query: () => void): Promise<BackendTimingInfo> {
+    query();
+    const kernelMs = await new Promise<number>(
+        resolve => setTimeout(
+            resolve(this.queryTimeMs * this.counter++), this.delayMs));
+    return {kernelMs};
+  }
+}
+
+class TestLogger extends Logger {
+  logKernelProfile(
+      name: string, result: Tensor, vals: TypedArray, timeMs: number) {}
+}
+
+describe('profiler.Profiler', () => {
+  it('profiles simple function', doneFn => {
+    const delayMs = 5;
+    const queryTimeMs = 10;
+    const timer = new TestBackendTimer(delayMs, queryTimeMs);
+    const logger = new TestLogger();
+    const profiler = new Profiler(timer, logger);
+
+    spyOn(timer, 'time').and.callThrough();
+    spyOn(logger, 'logKernelProfile').and.callThrough();
+
+    const timeSpy = timer.time as jasmine.Spy;
+    const logKernelProfileSpy = logger.logKernelProfile as jasmine.Spy;
+
+    let kernelCalled = false;
+    const result = 1;
+    const resultScalar = tf.scalar(result);
+
+    profiler.profileKernel('MatMul', () => {
+      kernelCalled = true;
+      return resultScalar;
+    });
+
+    setTimeout(() => {
+      expect(timeSpy.calls.count()).toBe(1);
+
+      expect(logKernelProfileSpy.calls.count()).toBe(1);
+
+      expect(logKernelProfileSpy.calls.first().args).toEqual([
+        'MatMul', resultScalar, new Float32Array([result]), queryTimeMs
+      ]);
+
+      expect(kernelCalled).toBe(true);
+      doneFn();
+    }, delayMs * 2);
+  });
+
+  it('profiles nested kernel', doneFn => {
+    const delayMs = 5;
+    const queryTimeMs = 10;
+    const timer = new TestBackendTimer(delayMs, queryTimeMs);
+    const logger = new TestLogger();
+    const profiler = new Profiler(timer, logger);
+
+    spyOn(timer, 'time').and.callThrough();
+    spyOn(logger, 'logKernelProfile').and.callThrough();
+    const timeSpy = timer.time as jasmine.Spy;
+    const logKernelProfileSpy = logger.logKernelProfile as jasmine.Spy;
+
+    let matmulKernelCalled = false;
+    let maxKernelCalled = false;
+    const result = 1;
+    const resultScalar = tf.scalar(result);
+
+    profiler.profileKernel('MatMul', () => {
+      const result = profiler.profileKernel('Max', () => {
+        maxKernelCalled = true;
+        return resultScalar;
+      });
+      matmulKernelCalled = true;
+      return result;
+    });
+
+    setTimeout(() => {
+      expect(timeSpy.calls.count()).toBe(2);
+
+      expect(logKernelProfileSpy.calls.count()).toBe(2);
+      expect(logKernelProfileSpy.calls.first().args).toEqual([
+        'Max', resultScalar, new Float32Array([result]), queryTimeMs
+      ]);
+      expect(logKernelProfileSpy.calls.argsFor(1)).toEqual([
+        'MatMul', resultScalar, new Float32Array([result]), queryTimeMs * 2
+      ]);
+
+      expect(matmulKernelCalled).toBe(true);
+      expect(maxKernelCalled).toBe(true);
+      doneFn();
+    }, delayMs * 2);
+  });
+});
+
+class TestBackendWebGLTimer implements BackendTimer {
   private counter = 1;
   constructor(
       private delayMs: number, private queryTimeMs: number,
@@ -43,7 +145,7 @@ class TestLogger extends Logger {
       gpuProgramsInfo?: GPUProgramsInfo[]) {}
 }
 
-describe('profiler.Profiler', () => {
+describeWithFlags('profiler.Profiler', WEBGL_ENVS, () => {
   it('profiles simple function', doneFn => {
     const delayMs = 5;
     const queryTimeMs = 10;
@@ -78,52 +180,6 @@ describe('profiler.Profiler', () => {
       ]);
 
       expect(kernelCalled).toBe(true);
-      doneFn();
-    }, delayMs * 2);
-  });
-
-  it('profiles nested kernel', doneFn => {
-    const delayMs = 5;
-    const queryTimeMs = 10;
-    const gpuProgramsInfo = [{name: '', ms: 0}];
-    const timer = new TestBackendTimer(delayMs, queryTimeMs, gpuProgramsInfo);
-    const logger = new TestLogger();
-    const profiler = new Profiler(timer, logger);
-
-    spyOn(timer, 'time').and.callThrough();
-    spyOn(logger, 'logKernelProfile').and.callThrough();
-    const timeSpy = timer.time as jasmine.Spy;
-    const logKernelProfileSpy = logger.logKernelProfile as jasmine.Spy;
-
-    let matmulKernelCalled = false;
-    let maxKernelCalled = false;
-    const result = 1;
-    const resultScalar = tf.scalar(result);
-
-    profiler.profileKernel('MatMul', () => {
-      const result = profiler.profileKernel('Max', () => {
-        maxKernelCalled = true;
-        return resultScalar;
-      });
-      matmulKernelCalled = true;
-      return result;
-    });
-
-    setTimeout(() => {
-      expect(timeSpy.calls.count()).toBe(2);
-
-      expect(logKernelProfileSpy.calls.count()).toBe(2);
-      expect(logKernelProfileSpy.calls.first().args).toEqual([
-        'Max', resultScalar, new Float32Array([result]), queryTimeMs,
-        gpuProgramsInfo
-      ]);
-      expect(logKernelProfileSpy.calls.argsFor(1)).toEqual([
-        'MatMul', resultScalar, new Float32Array([result]), queryTimeMs * 2,
-        gpuProgramsInfo
-      ]);
-
-      expect(matmulKernelCalled).toBe(true);
-      expect(maxKernelCalled).toBe(true);
       doneFn();
     }, delayMs * 2);
   });
