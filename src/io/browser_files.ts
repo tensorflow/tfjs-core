794ab4f65d883a2813986ab4661d57f5c33c32b2
Kinar R <ranik40@gmail/com>
2018-10-31 00:06:27 +0530

    != changed to !==

--- src/io/browser_files.ts
+++ src/io/browser_files.ts
@@ -88,7 +88,7 @@ export class BrowserDownloads implements IOHandler {
       jsonAnchor.href = modelTopologyAndWeightManifestURL;
       // Trigger downloads by calling the `click` methods on the download
       // anchors.
-      if(navigator.userAgent.indexOf('Firefox') != -1) {
+      if(navigator.userAgent.indexOf('Firefox') !== -1) {
         jsonAnchor.dispatchEvent(new MouseEvent('click'));
       }
       else {
@@ -101,7 +101,7 @@ export class BrowserDownloads implements IOHandler {
             this.weightDataAnchor;
         weightDataAnchor.download = this.weightDataFileName;
         weightDataAnchor.href = weightsURL;
-        if(navigator.userAgent.indexOf('Firefox') != -1) {
+        if(navigator.userAgent.indexOf('Firefox') !== -1) {
           weightDataAnchor.dispatchEvent(new MouseEvent('click'));
         }
         else {
