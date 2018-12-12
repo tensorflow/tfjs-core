d173b142518df84f800d40cc0fe53b2b6095a6cb
rthadur <>
2018-12-11 23:25:31 -0800

    added helper method
    added helper method

--- src/io/browser_files.ts
+++ src/io/browser_files.ts
@@ -88,16 +88,7 @@ export class BrowserDownloads implements IOHandler {
       jsonAnchor.href = modelTopologyAndWeightManifestURL;
       // Trigger downloads by calling the `click` methods on the download
       // anchors.
-      if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-        const evt = document.createEvent('MouseEvents');
-        evt.initMouseEvent(
-            'click', true, true, window, 0, 0, 0, 0, 0, false, false, false,
-            false, 0, null);
-        jsonAnchor.dispatchEvent(evt);
-      } else {
-        jsonAnchor.click();
-      }
-
+      downloadFile(jsonAnchor);
 
       if (modelArtifacts.weightData != null) {
         const weightDataAnchor = this.weightDataAnchor == null ?
@@ -105,22 +96,24 @@ export class BrowserDownloads implements IOHandler {
             this.weightDataAnchor;
         weightDataAnchor.download = this.weightDataFileName;
         weightDataAnchor.href = weightsURL;
-        if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-          const evt = document.createEvent('MouseEvents');
-          evt.initMouseEvent(
-              'click', true, true, window, 0, 0, 0, 0, 0, false, false, false,
-              false, 0, null);
-          weightDataAnchor.dispatchEvent(evt);
-        } else {
-          weightDataAnchor.click();
-        }
+        downloadFile(weightDataAnchor);
       }
 
       return {modelArtifactsInfo: getModelArtifactsInfoForJSON(modelArtifacts)};
     }
   }
 }
-
+function downloadFile(anchor: HTMLAnchorElement) {
+  if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+    const evt = document.createEvent('MouseEvents');
+    evt.initMouseEvent(
+        'click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false,
+        0, null);
+    if (anchor != null) anchor.dispatchEvent(evt);
+  } else {
+    anchor.click();
+  }
+};
 class BrowserFiles implements IOHandler {
   private readonly files: File[];
 
