11ee8f240ab4d4d77b1043f0e4e39bd98c145879
Ann Yuan <annyuan@google.com>
2018-12-28 14:36:07 -0500

    increase timeout

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function(config) {
     },
     captureTimeout: 120000,
     reportSlowerThan: 500,
-    browserNoActivityTimeout: 180000,
+    browserNoActivityTimeout: 250000,
     customLaunchers: {
       bs_chrome_mac: {
         base: 'BrowserStack',
