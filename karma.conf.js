4affa8765680125d61a99ad63073f13c6ee48268
Ann Yuan <annyuan@google.com>
2019-01-02 13:45:50 -0500

    shorter timeout

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,7 @@ module.exports = function(config) {
     captureTimeout: 120000,
     reportSlowerThan: 500,
     browserNoActivityTimeout: 1000000,
-    browserDisconnectTimeout: 2000000,
+    browserDisconnectTimeout: 20000,
     customLaunchers: {
       bs_chrome_mac: {
         base: 'BrowserStack',
