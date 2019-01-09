763f0d2095f4cea87336fdd117a28e7ffbcb1ab4
Ann Yuan <annyuan@google.com>
2019-01-09 10:28:03 -0500

    add more tests

--- src/ops/binary_ops_test.ts
+++ src/ops/binary_ops_test.ts
@@ -264,6 +264,19 @@ describeWithFlags('maximum', ALL_ENVS, () => {
         .toThrowError(
             /Argument 'b' passed to 'maximum' must be numeric tensor/);
   });
+
+  it('works with squarification for large dimension', () => {
+    const maxTextureSize = tf.ENV.get('WEBGL_MAX_TEXTURE_SIZE');
+    tf.ENV.set('WEBGL_MAX_TEXTURE_SIZE', 5);
+    const a =
+        tf.tensor2d([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [2, 7]);
+    const b =
+        tf.tensor2d([-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [2, 7]);
+
+    const result = tf.maximum(a, b);
+    tf.ENV.set('WEBGL_MAX_TEXTURE_SIZE', maxTextureSize);
+    expectArraysClose(result, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
+  });
 });
 
 describeWithFlags('squaredDifference', ALL_ENVS, () => {
--- src/ops/unary_ops_test.ts
+++ src/ops/unary_ops_test.ts
@@ -66,6 +66,16 @@ describeWithFlags('relu', ALL_ENVS, () => {
     expectArraysClose(result, [1, 0, 0, 3, 0, NaN]);
   });
 
+  it('works with squarification for prime number length vector', () => {
+    const maxTextureSize = tf.ENV.get('WEBGL_MAX_TEXTURE_SIZE');
+    tf.ENV.set('WEBGL_MAX_TEXTURE_SIZE', 5);
+    const a = tf.tensor1d([1, -2, 5, -3, -1, 4, 7]);
+    const result = tf.relu(a);
+
+    tf.ENV.set('WEBGL_MAX_TEXTURE_SIZE', maxTextureSize);
+    expectArraysClose(result, [1, 0, 5, 0, 0, 4, 7]);
+  });
+
   it('gradients: positive scalar', () => {
     const a = tf.scalar(3);
     const dy = tf.scalar(5);
