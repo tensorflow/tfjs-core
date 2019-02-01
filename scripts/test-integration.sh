32017fc68dafd0614fb5c51c043a918148ec2a0d
Nikhil Thorat <nsthorat@google.com>
2019-02-01 11:49:13 -0500

    save

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -48,16 +48,25 @@ function test () {
   yarn build && yarn lint && yarn test-travis
   CONVERTER_EXIT_CODE=$?
 
+  cd ..
+  echo 'Cloning data'
+  git clone https://github.com/tensorflow/tfjs-data.git --depth 5
+  cd tfjs-data
+  yarn && yarn link-local '@tensorflow/tfjs-core'
+  yarn build && yarn lint && yarn test-travis
+  DATA_EXIT_CODE=$?
+
   echo '==== INTEGRATION TEST RESULTS ===='
   print_status "tfjs-layers" "$LAYERS_EXIT_CODE"
   print_status "tfjs-node" "$NODE_EXIT_CODE"
   print_status "tfjs-converter" "$CONVERTER_EXIT_CODE"
+  print_status "tfjs-data" "$DATA_EXIT_CODE"
   echo '=================================='
 
   RED='\033[0;31m'
   BLUE='\e[34m'
   NC='\033[0m' # No Color
-  FINAL_EXIT_CODE=$(($LAYERS_EXIT_CODE+$NODE_EXIT_CODE+$CONVERTER_EXIT_CODE))
+  FINAL_EXIT_CODE=$(($LAYERS_EXIT_CODE+$NODE_EXIT_CODE+$CONVERTER_EXIT_CODE+$DATA_EXIT_CODE))
   [[ $FINAL_EXIT_CODE -eq 0 ]] && COLOR=$BLUE || COLOR=$RED
   print_status "${COLOR}Final result" "$FINAL_EXIT_CODE" "$NC"
   exit $FINAL_EXIT_CODE
@@ -67,6 +76,7 @@ function test () {
 readarray -t files_changed <<< "$(git diff --name-only $TRAVIS_COMMIT_RANGE)"
 
 for file in "${files_changed[@]}"
+test
 do
   if [ "$file" = "src/version.ts" ]; then test; break; fi
 done
