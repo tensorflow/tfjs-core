802bf3e775698bfcd5a8eb800b254dab96d0902a
Daniel Smilkov <dsmilkov@gmail.com>
2019-02-22 14:57:09 -0500

    save

--- worker/worker.js
+++ worker/worker.js
@@ -1,24 +1,57 @@
 function ASMModule(stdlib, _, heap) {
   "use asm";
 
+  var fround = stdlib.Math.fround;
   var imul = stdlib.Math.imul;
+  var heap32 = new stdlib.Float32Array(heap);
+  
 
-  function square(aSize, bSize, mid) {
+  function matmul(aSize, bSize, mid) {
+    // Function arguments.
     aSize = aSize | 0;
     bSize = bSize | 0;
     mid = mid | 0;
 
-    const a = new Float32Array(heap, 0, aSize * mid);
-    const b = new Float32Array(heap, a.length * a.BYTES_PER_ELEMENT, bSize * mid);
-    for (let i = 0; (i|0) < (aSize|0); i = (i + 1)|0) {
+    // Variable declaration.
+    var offset = 0;
+    var i = 0;
+    var j = 0;
+    var k = 0;
+    var bOffset = 0;
+    var cOffset = 0;
+    var a = fround(0);
+    var b = fround(0);
+    var dot = fround(0);
 
+    bOffset = imul(aSize, mid) << 2;
+    cOffset = imul(bSize, mid) << 2;
+  
+    for (i = 0; (i|0) < (bOffset|0); i = (i + 4)|0) {
+      for (j = 0; (j|0) < (cOffset|0); j = (j + 4)|0) {
+        dot = fround(0);
+        for (k = 0; (k|0) < (mid << 2); k = (k + 4)|0) {
+          ;
+          //a = // aVals[i * mid + k]
+        }
+      }
+      // read A.
+      offset = i;
+      a = fround(heap32[offset >> 2]);
+      
+      // read B.
+      offset = (offset + bOffset)|0;
+      b = fround(heap32[offset >> 2]);
+      
+      // write to C.
+      offset = (offset + cOffset)|0;
+      heap32[offset >> 2] = fround(a * b);
     }
   }
-
-  return {square: square};
+  return {matmul: matmul};
 }
 
-var heap = new ArrayBuffer(0x10000); 
+var heap = new ArrayBuffer(128 * 1024); // 128k heap
+var heapF32 = new Float32Array(heap);
 var asm = ASMModule(self, null, heap);
 
 onmessage = function(e) {
@@ -26,17 +59,22 @@ onmessage = function(e) {
   const aSize = aVals.length / mid;
   const bSize = bVals.length / mid;
 
-  console.log(asm.square(3));
-
-  const res = new Float32Array(aSize * bSize);
-  for (let i = 0; i < aSize; ++i) {
-    for (let j = 0; j < bSize; ++j) {
-      let dot = 0;
-      for (let k = 0; k < mid; ++k) {
-        dot += aVals[i * mid + k] * bVals[k * bSize + j];
-      }
-      res[i * bSize + j] = dot;
-    }
-  }
+  heapF32.set(aVals, 0);
+  heapF32.set(bVals, aVals.length);
+  console.log(heapF32);
+  asm.matmul(aSize, bSize, mid);
+  const offset = aVals.length + bVals.length;
+  const res = heapF32.slice(offset, offset + aSize * bSize);
+  console.log('Result', res);
+  // const res = new Float32Array(aSize * bSize);
+  // for (let i = 0; i < aSize; ++i) {
+  //   for (let j = 0; j < bSize; ++j) {
+  //     let dot = 0;
+  //     for (let k = 0; k < mid; ++k) {
+  //       dot += aVals[i * mid + k] * bVals[k * bSize + j];
+  //     }
+  //     res[i * bSize + j] = dot;
+  //   }
+  // }
   postMessage(res);
 }
