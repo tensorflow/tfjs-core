4d27c96da32b7c55371766a887263e8732564b07
Daniel Smilkov <dsmilkov@gmail.com>
2019-02-22 12:31:17 -0500

    save

--- worker/worker.js
+++ worker/worker.js
@@ -1,9 +1,33 @@
+function ASMModule(stdlib, _, heap) {
+  "use asm";
+
+  var imul = stdlib.Math.imul;
+
+  function square(aSize, bSize, mid) {
+    aSize = aSize | 0;
+    bSize = bSize | 0;
+    mid = mid | 0;
+
+    const a = new Float32Array(heap, 0, aSize * mid);
+    const b = new Float32Array(heap, a.length * a.BYTES_PER_ELEMENT, bSize * mid);
+    for (let i = 0; (i|0) < (aSize|0); i = (i + 1)|0) {
+
+    }
+  }
+
+  return {square: square};
+}
+
+var heap = new ArrayBuffer(0x10000); 
+var asm = ASMModule(self, null, heap);
+
 onmessage = function(e) {
-  const [aBytes, bBytes, mid] = e.data;
-  const aVals = new Float32Array(aBytes);
-  const bVals = new Float32Array(bBytes);
+  const [aVals, bVals, mid] = e.data;
   const aSize = aVals.length / mid;
   const bSize = bVals.length / mid;
+
+  console.log(asm.square(3));
+
   const res = new Float32Array(aSize * bSize);
   for (let i = 0; i < aSize; ++i) {
     for (let j = 0; j < bSize; ++j) {
