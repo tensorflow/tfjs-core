b151be58d0063e4f8440f9bcf749e8367fc44981
Josh Gartman <joshgartman@gmail.com>
2019-03-15 22:24:02 -0400

    Review comments

--- src/kernels/webgl/reverse_packed_gpu.ts
+++ src/kernels/webgl/reverse_packed_gpu.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2019 Google Inc. All Rights Reserved.
+ * Copyright 2019 Google LLC All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -34,47 +34,74 @@ export class ReversePackedProgram implements GPGPUProgram {
     this.outputShape = xShape;
     const channels = getChannels('rc', rank);
     const nextColumn =
-        `++${channels[rank - 1]} < ${this.outputShape[rank - 1]}`;
-    const getInCoord = (i: number) => {
-      if (rank === 1) {
-        return `${xShape[0]} - rc - 1`;
-      } else if (axis.indexOf(i) !== -1 && xShape[i] !== 1) {
-        return `${xShape[i]} - rc[${i}] - 1`;
-      } else {
-        return `rc[${i}]`;
-      }
-    };
-    const inCoords = xShape.map((_, i) => getInCoord(i)).join(',');
-    const innerDims = rank === 1 ?
-        'rc' :
-        `vec2(${xShape.map((_, i) => getInCoord(i)).slice(-2).join(',')})`;
+        `${channels[rank - 1]} + 1 < ${this.outputShape[rank - 1]}`;
+    const nextRow = `${channels[rank - 2]} + 1 < ${this.outputShape[rank - 2]}`;
     const type = getCoordsDataType(rank);
-    const getc = `getChannel(getX(${inCoords}), ${innerDims})`;
-
-    const upperRow = `
-      result.x = ${getc};
-      if(${nextColumn}){
-        result.y = ${getc};
-      }
-    `;
-    const lowerRow = rank === 1 ? '' : `
-      --${channels[rank - 1]};
-      if (++${channels[rank - 2]} < ${this.outputShape[rank - 2]}) {
-        result.z = ${getc};
-        if(${nextColumn}) {
-          result.w = ${getc};
+    if (rank === 1) {
+      this.userCode = `
+        void main(){
+          int rc = getOutputCoords();
+          vec4 result = vec4(0.);
+          result.r = getChannel(getX(${xShape[0]} - rc - 1), rc);
+          if(${nextColumn}){
+              result.g = getChannel(getX(${xShape[0]} - (rc  + 1) - 1), rc + 1);
+          }
+          setOutput(result);
         }
-      }
+      `;
+    } else {
+      this.userCode = `
+        void main() {
+          ${type} rc = getOutputCoords();
+          vec4 result = vec4(0.);
+          result.r = ${getR(channels.slice())};
+          if(${nextColumn}){
+            result.g = ${getG(channels.slice())};
+          }
+          if(${nextRow}) {
+            result.b = ${getB(channels.slice())};
+            if(${nextColumn}) {
+              result.a = ${getA(channels.slice())};
+            }
+          }
+          setOutput(result);
+        }
     `;
+    }
 
-    this.userCode = `
-      void main() {
-      ${type} rc = getOutputCoords();
-      vec4 result = vec4(0.);
-      ${upperRow};
-      ${lowerRow};
-      setOutput(result);
+    function getR(channels: string[]): string {
+      return getChannel(channels);
+    }
+
+    function getG(channels: string[]): string {
+      channels[rank - 1] = '(' + channels[rank - 1] + ` + 1)`;
+      return getChannel(channels);
+    }
+
+    function getB(channels: string[]): string {
+      channels[rank - 2] = '(' + channels[rank - 2] + ` + 1)`;
+      return getChannel(channels);
+    }
+
+    function getA(channels: string[]): string {
+      channels[rank - 1] = '(' + channels[rank - 1] + ` + 1)`;
+      channels[rank - 2] = '(' + channels[rank - 2] + ` + 1)`;
+      return getChannel(channels);
+    }
+
+    function getChannel(channels: string[]): string {
+      const inCoordsArray = xShape.map((_, i) => getInCoord(i, channels));
+      const inCoords = inCoordsArray.join(',');
+      const innerDims = inCoordsArray.slice(-2).join(',');
+      return `getChannel(getX(${inCoords}), vec2(${innerDims}))`;
+    }
+
+    function getInCoord(i: number, channels1: string[]): string {
+      if (axis.indexOf(i) !== -1 && xShape[i] !== 1) {
+        return `${xShape[i]} - ${channels1[i]} - 1`;
+      } else {
+        return `${channels1[i]}`;
       }
-    `;
+    }
   }
 }
