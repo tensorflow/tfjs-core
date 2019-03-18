108f0e10fb7b1138068f598c7231a01ec4332e52
Ping Yu <4018+pyu10055@users.noreply.github.com>
2019-03-18 13:34:52 -0700

    separate the test to three cases

--- src/ops/strided_slice_test.ts
+++ src/ops/strided_slice_test.ts
@@ -270,12 +270,31 @@ describeWithFlags('stridedSlice', ALL_ENVS, () => {
     expectArraysClose(output, [1, 2, 3, 4, 5, 6]);
   });
 
-  it('stridedSlice should support 3d with smaller length of begin/end array',
+  it('stridedSlice should support 3d with smaller length of begin array',
      () => {
        const tensor =
            tf.tensor4d([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [2, 3, 1, 2]);
-       const output =
-           tf.stridedSlice(tensor, [1, 0], [2, 3], [1, 1], 0, 0, 0, 0, 0);
+       const output = tf.stridedSlice(
+           tensor, [1, 0], [2, 3, 1, 2], [1, 1, 1, 1], 0, 0, 0, 0, 0);
+       expect(output.shape).toEqual([1, 3, 1, 2]);
+       expectArraysClose(output, [7, 8, 9, 10, 11, 12]);
+     });
+
+  it('stridedSlice should support 3d with smaller length of end array', () => {
+    const tensor =
+        tf.tensor4d([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [2, 3, 1, 2]);
+    const output = tf.stridedSlice(
+        tensor, [1, 0, 0, 0], [2, 3], [1, 1, 1, 1], 0, 0, 0, 0, 0);
+    expect(output.shape).toEqual([1, 3, 1, 2]);
+    expectArraysClose(output, [7, 8, 9, 10, 11, 12]);
+  });
+
+  it('stridedSlice should support 3d with smaller length of stride array',
+     () => {
+       const tensor =
+           tf.tensor4d([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [2, 3, 1, 2]);
+       const output = tf.stridedSlice(
+           tensor, [1, 0, 0, 0], [2, 3, 1, 2], [1, 1], 0, 0, 0, 0, 0);
        expect(output.shape).toEqual([1, 3, 1, 2]);
        expectArraysClose(output, [7, 8, 9, 10, 11, 12]);
      });
