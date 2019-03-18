a9cc1935cd004aba1efcf3c73ad23dc015b96a58
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-03-18 10:16:02 -0400

    remove unnecessary comment

--- src/ops/pool_test.ts
+++ src/ops/pool_test.ts
@@ -19,24 +19,6 @@ import * as tf from '../index';
 import {describeWithFlags} from '../jasmine_util';
 import { ALL_ENVS, CPU_ENVS, expectArraysClose} from '../test_util';
 
-// describeWithFlags('maxPool3D', CPU_ENVS, () => {
-//   it('x=[1,4,4,4,1] f=[2,2,2] s=2', () => {
-//     // Feed forward.
-//     const x = tf.tensor5d(
-//       [0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12.,
-//         13., 14., 15., 16., 17., 18., 19., 20., 21., 22., 23., 24., 25.,
-//         26., 27., 28., 29., 30., 31., 32., 33., 34., 35., 36., 37., 38.,
-//         39., 40., 41., 42., 43., 44., 45., 46., 47., 48., 49., 50., 51.,
-//         52., 53., 54., 55., 56., 57., 58., 59., 60., 61., 62., 63.],
-//         [1, 4, 4, 4, 1]);
-
-//     const result = tf.maxPool3D(x, 2, 2, 'same');
-
-//     expect(result.shape).toEqual([1, 2, 2, 2, 1]);
-//     expectArraysClose(result, [21., 23., 29., 31., 53., 55., 61., 63.]);
-//   });
-// });
-
 describeWithFlags('maxPool3D', CPU_ENVS, () => {
   // ```python
   // x = np.ones((1, 1, 1, 1, 1), dtype=np.float32)
