7995656118aea363bbb2a89f10f93777a638b60b
Ann Yuan <annyuan@google.com>
2019-04-16 07:04:25 -0400

    pr comments

--- src/backends/webgpu/README.md
+++ src/backends/webgpu/README.md
@@ -1,3 +1,10 @@
+This is an experimental backend.
+
+```
+    $ yarn # to install dependencies
+    $ yarn test
+```
+
 # To run the test suite:
 The `$CHROME_BIN` environment variable must be set to the location of a custom Chromium build. 
 
--- src/backends/webgpu/rollup.config.js
+++ src/backends/webgpu/rollup.config.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
+ * Copyright 2019 Google LLC. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -34,9 +34,6 @@ function config({plugins = [], output = {}, external = []}) {
       commonjs({
         ignore: ['crypto'],
         include: 'node_modules/**',
-        namedExports: {
-          './node_modules/seedrandom/index.js': ['alea'],
-        },
       }),
       ...plugins
     ],
@@ -45,7 +42,6 @@ function config({plugins = [], output = {}, external = []}) {
       sourcemap: true,
       ...output,
     },
-    external: ['crypto', ...external],
     onwarn: warning => {
       let {code} = warning;
       if (code === 'CIRCULAR_DEPENDENCY' || code === 'CIRCULAR' ||
