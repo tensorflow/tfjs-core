35593d7a7fac1587393eeefb8166ec5d59e07682
Nikhil Thorat <nsthorat@google.com>
2019-04-29 16:24:28 -0400

    save

--- scripts/test_snippets/test_snippets.ts
+++ scripts/test_snippets/test_snippets.ts
@@ -15,46 +15,60 @@
  * =============================================================================
  */
 
-import * as tf from '@tensorflow/tfjs';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as ts from 'typescript';
 
+import * as tf from '../../src/index';
+// tf is used in the eval() but not visible to this file so we ignore the unused
+// expression.
+// tslint:disable-next-line:no-unused-expression
+[tf];
+
 const index = path.join(process.cwd(), 'src/index.ts');
 const tsconfigPath = path.join(process.cwd(), 'tsconfig.json');
-console.log(index);
 
 // Use the same compiler options that we use to compile the library
 // here.
 const tsconfig = JSON.parse(fs.readFileSync(tsconfigPath, 'utf8'));
 
 delete tsconfig.compilerOptions.moduleResolution;
-// Build a program using the set of root file names in fileNames
 const program = ts.createProgram([index], tsconfig.compilerOptions);
 
-// Get the checker, we will use it to find more about classes
 const checker = program.getTypeChecker();
 
+let snippetCount = 0;
 async function main() {
-  // Visit every sourceFile in the program
   for (const sourceFile of program.getSourceFiles()) {
     if (!sourceFile.isDeclarationFile) {
-      // Walk the tree to search for classes
-      // ts.forEachChild(sourceFile, visit);
       const children = sourceFile.getChildren();
-      console.log(children.length);
       for (let i = 0; i < children.length; i++) {
         await visit(children[i]);
       }
     }
   }
+
+  console.log(`Parsed and evaluated ${snippetCount} snippets successfully.`);
 }
 
-/** visit nodes finding exported classes */
 async function visit(node: ts.Node) {
+  const children = node.getChildren();
+  for (let i = 0; i < children.length; i++) {
+    await visit(children[i]);
+  }
+
   if (ts.isClassDeclaration(node) || ts.isFunctionDeclaration(node) ||
       ts.isMethodDeclaration(node) || ts.isInterfaceDeclaration(node)) {
     const symbol = checker.getSymbolAtLocation(node.name);
+    const jsdoc = getJSDoc(symbol);
+    if (jsdoc == null) {
+      return;
+    }
+    // Ignore snippets of methods that have been marked with ignoreCI.
+    if (jsdoc['ignoreCI']) {
+      return;
+    }
+
     const documentation = symbol.getDocumentationComment(checker);
     if (documentation == null) {
       return;
@@ -68,9 +82,10 @@ async function visit(node: ts.Node) {
       }
 
       for (let k = 0; k < matches.length; k++) {
+        snippetCount++;
+
         const match = matches[k];
         const lines = match.split('\n');
-        console.log('-------------');
         const evalLines: string[] = [];
         for (let j = 0; j < lines.length; j++) {
           let line = lines[j];
@@ -88,20 +103,58 @@ async function visit(node: ts.Node) {
         }
 
         const srcCode = evalLines.join('\n');
-        console.log(srcCode);
-        await tf.nextFrame();
+
+        const reportError = (e: string|Error) => {
+          console.log(symbol.name, jsdoc);
+          console.log(srcCode);
+          if (e instanceof Error) {
+            throw (e);
+          }
+          throw new Error(e);
+        };
+
         const evalString = '(async function runner() { try { ' + srcCode +
             '} catch (e) { reportError(e); } })()';
-        eval(evalString);
 
-        console.log('...............');
+        const oldLog = [console.log, console.info];
+
+        // Overrwrite console.log so we don't spam the console.
+        console.log = (msg: string) => {};
+        console.warn = (msg: string) => {};
+        try {
+          await eval(evalString).catch((e: Error) => {
+            reportError(e);
+          });
+        } catch (e) {
+          reportError(e);
+        }
+        console.log = oldLog[0];
+        console.warn = oldLog[1];
       }
     }
   }
-  const children = node.getChildren();
-  for (let i = 0; i < children.length; i++) {
-    await visit(children[i]);
-  }
 }
 
+interface JSDoc {
+  namespace?: string;
+  ignoreCI?: boolean;
+}
+
+function getJSDoc(symbol: ts.Symbol): JSDoc {
+  const tags = symbol.getJsDocTags();
+  for (let i = 0; i < tags.length; i++) {
+    const jsdocTag = tags[i];
+    if (jsdocTag.name === 'doc' && jsdocTag.text != null) {
+      const json = convertDocStringToDocInfoObject(jsdocTag.text.trim());
+      return json;
+    }
+  }
+  return null;
+}
+
+function convertDocStringToDocInfoObject(docString: string): JSDoc {
+  const jsonString =
+      docString.replace(/([a-zA-Z0-9]+):/g, '"$1":').replace(/\'/g, '"');
+  return JSON.parse(jsonString);
+}
 main();
