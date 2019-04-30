68730843c3c35e5ef33dde3e4ad31ef366ad9a65
Nikhil Thorat <nsthorat@google.com>
2019-04-30 17:12:01 -0400

    save

--- src/tests.ts
+++ src/tests.ts
@@ -111,6 +111,7 @@ import './optimizers/optimizer_test';
 import './optimizers/rmsprop_optimizer_test';
 import './optimizers/sgd_optimizer_test';
 import './platforms/browser/platform_browser_test';
+import './platforms/node/platform_node_test';
 import './profiler_test';
 import './serialization_test';
 import './tape_test';
