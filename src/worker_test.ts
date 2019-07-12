c3897b7c158d787aca8d9f23b5958b1c1271d27e
WenheLI <wl1508@nyu.edu>
2019-07-12 14:02:59 +0800

    Fix lint

--- src/worker_test.ts
+++ src/worker_test.ts
@@ -2,21 +2,21 @@ import {ALL_ENVS, describeWithFlags} from './jasmine_util';
 import {expectArraysClose} from './test_util';
 import * as tf from './index';
 
-let workerTest = () => {
+const workerTest = () => {
 	//@ts-ignore
-	importScripts('http://bs-local.com:9876/base/dist/tf-core.js')
+	importScripts('http://bs-local.com:9876/base/dist/tf-core.js');
 	let a = tf.tensor1d([1, 2, 3]);
-	let b = tf.tensor1d([3, 2, 1]);
+	const b = tf.tensor1d([3, 2, 1]);
 	a = a.add(b);
 	//@ts-ignore
 	self.postMessage({data: a.dataSync()});
-}
+};
 
-let fn2workerURL = (fn: Function): string => {
-	var blob =
+const fn2workerURL = (fn: Function): string => {
+	const blob =
 			new Blob(['('+fn.toString()+')()'], {type: 'application/javascript'});
   return URL.createObjectURL(blob);
-}
+};
 
 describeWithFlags('computation in worker', ALL_ENVS, () => {
   it('tensor in worker', (done) => {
