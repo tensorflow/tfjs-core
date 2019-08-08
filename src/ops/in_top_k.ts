f47968bd9f76433ce6df2395b9856141fb055056
Nikhil Thorat <nsthorat@google.com>
2019-08-08 09:52:49 -0400

    Cleanup intopk to remove it as a kernel.

--- src/ops/in_top_k.ts
+++ src/ops/in_top_k.ts
@@ -15,13 +15,11 @@
  * =============================================================================
  */
 
-import {ENGINE} from '../engine';
-import {NumericTensor, Tensor} from '../tensor';
+import {Tensor} from '../tensor';
 import {convertToTensor} from '../tensor_util_env';
 import {TensorLike} from '../types';
-import {assert, assertShapesMatch} from '../util';
-
-import {op} from './operation';
+import {assert, assertShapesMatch, getTypedArrayFromDType} from '../util';
+import {tensor} from './tensor_ops';
 
 /**
  * Says whether the targets are in the top K predictions.
@@ -39,8 +37,8 @@ import {op} from './operation';
  *     default to 1.
  */
 /** @doc {heading: 'Operations', subheading: 'Evaluation'} */
-function inTopK_<T extends Tensor, U extends Tensor>(
-    predictions: T|TensorLike, targets: U|TensorLike, k = 1): U {
+async function inTopKAsync_<T extends Tensor, U extends Tensor>(
+    predictions: T|TensorLike, targets: U|TensorLike, k = 1): Promise<U> {
   const $predictions = convertToTensor(predictions, 'predictions', 'inTopK');
   const $targets = convertToTensor(targets, 'targets', 'inTopK');
 
@@ -50,9 +48,9 @@ function inTopK_<T extends Tensor, U extends Tensor>(
           `but got ${$predictions.rank}`);
   assert(
       $predictions.rank - 1 === $targets.rank,
-      () => `predictions' rank should be 1 larger than ` +
-          `targets' rank, but got predictions' rank ` +
-          `${$predictions.rank} and targets' rank ${$targets.rank}`);
+      () => `predictions rank should be 1 larger than ` +
+          `targets rank, but got predictions rank ` +
+          `${$predictions.rank} and targets rank ${$targets.rank}`);
   assertShapesMatch(
       $predictions.shape.slice(0, $predictions.shape.length - 1),
       $targets.shape,
@@ -61,15 +59,44 @@ function inTopK_<T extends Tensor, U extends Tensor>(
   const lastDim = $predictions.shape[$predictions.shape.length - 1];
   assert(
       k > 0 && k <= lastDim,
-      () => `'k' passed to inTopK() must be > 0 && <= the predictions' last ` +
+      () => `'k' passed to inTopK() must be > 0 && <= the predictions last ` +
           `dimension (${lastDim}), but got ${k}`);
 
-  const precision = ENGINE.runKernel(
-      b =>
-          b.inTopK($predictions as NumericTensor, $targets as NumericTensor, k),
-      {$predictions, $targets});
+  const predictionsVals = await $predictions.data();
+  const targetsVals = await $targets.data();
 
-  return precision as U;
+  // Reshape predictionsVals into a 2d tensor [batch, lastDim]
+  // and look up topK along lastDim.
+  const [batch, size] = [predictionsVals.length / lastDim, lastDim];
+  const precision = getTypedArrayFromDType('bool', batch);
+
+  for (let b = 0; b < batch; b++) {
+    const offset = b * size;
+    const vals = predictionsVals.subarray(offset, offset + size);
+    const valAndInd: Array<{value: number, index: number}> = [];
+    for (let i = 0; i < vals.length; i++) {
+      valAndInd.push({value: vals[i], index: i});
+    }
+    valAndInd.sort((a, b) => b.value - a.value);
+
+    precision[b] = 0;
+    for (let i = 0; i < k; i++) {
+      if (valAndInd[i].index === targetsVals[b]) {
+        precision[b] = 1;
+        break;
+      }
+    }
+  }
+
+  if (predictions !== $predictions) {
+    $predictions.dispose();
+  }
+  if (targets !== $targets) {
+    $targets.dispose();
+  }
+
+  // Output precision has the same shape as targets.
+  return tensor(precision, $targets.shape, 'bool') as U;
 }
 
-export const inTopK = op({inTopK_});
+export const inTopKAsync = inTopKAsync_;
