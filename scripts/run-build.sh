45b84314fccbad8cb21cf4202d6e896c05185811
Daniel Smilkov <dsmilkov@gmail.com>
2019-08-12 16:00:36 -0400

    save

--- scripts/run-build.sh
+++ scripts/run-build.sh
@@ -18,5 +18,6 @@ set -e
 
 DIR=$1
 if test -f "$DIR/diff"; then
-  gcloud builds submit $DIR --config=$DIR/cloudbuild.yml
+  cp tslint.json $DIR/tslint.json
+  gcloud builds submit . --config=$DIR/cloudbuild.yml
 fi
